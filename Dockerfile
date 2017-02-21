122bd150d30434a2226a357ef8146af03b03565b
Adam Kariv <adam.kariv@gmail.com>
2017-02-21 18:06:27 +0200

    Association Registry

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM frictionlessdata/datapackage-pipelines:latest
 
 ADD pipelines /pipelines
+ADD requirements.txt /
 
 RUN apk add --update git && pip install -U git+https://github.com/frictionlessdata/tabulator-py
 RUN addgroup dpp && adduser -s /bin/bash -D -G dpp dpp && \
     mkdir -p /var/datapackages && chown dpp.dpp /var/datapackages -R && \
     chown dpp.dpp /pipelines -R
+RUN pip install -r /requirements.txt
 USER dpp
 
 EXPOSE 5000
---
+++ pipelines/entities/associations/all_associations.py
@@ -0,0 +1,32 @@
+import demjson
+import requests
+
+from datapackage_pipelines.wrapper import ingest, spew
+
+params, datapackage, res_iter = ingest()
+
+
+def get_associations():
+    all_db_url = 'http://www.justice.gov.il/DataGov/Corporations/Associations.json.txt'
+    all_db = requests.get(all_db_url).content
+    all_db = demjson.decode(all_db)
+    for rec in all_db['Data']:
+        yield dict(
+            (k, str(v)) for k, v in rec.items()
+        )
+
+resource = {
+    'name': 'associations',
+    'path': 'data/associations.csv',
+    'schema': {
+        'fields': [
+            {'name': 'Association_Number', 'type': 'string'},
+            {'name': 'Association_Name', 'type': 'string'},
+            {'name': 'Association_Registration_Date', 'type': 'string'},
+        ]
+    }
+}
+
+datapackage['resources'].append(resource)
+
+spew(datapackage, [get_associations()])
