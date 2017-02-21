125b48aed15388b8134d87a94ccb9573df257681
Adam Kariv <adam.kariv@gmail.com>
2017-02-21 20:39:50 +0200

    Guidestar scraping

---
+++ pipelines/entities/associations/guidestar/scraper.py
@@ -0,0 +1,76 @@
+import requests
+from pyquery import PyQuery as pq
+import time
+
+from datapackage_pipelines.wrapper import ingest, spew
+
+parameters, datapackage, res_iter = ingest()
+
+classes = {
+    '.views-field-title-field': 'title',
+    '.views-field-field-gov-objectives.has-text > .field-content': 'objective',
+    '.views-field-php-1 > .field-content': 'address',
+    '#block-views-organization-page-block-2 .views-field-php > .field-content': 'org_kind',
+    '#block-views-organization-page-block-2 .views-field-field-gov-year-established > .field-content': 'year_established',
+    '#block-views-organization-page-block-2 .views-field-field-gov-proper-management > .field-content': 'proper_management',
+    '.field-collection-item-field-gov-founder .content': 'founders[]',
+    '#nonononono': 'id'
+}
+
+
+def scrape_guidestar(all_ids):
+    for i, ass_id in enumerate(all_ids):
+        guidestar_url = 'http://www.guidestar.org.il/he/organization/{}'.format(ass_id)
+
+        page = None
+        while page is None:
+            try:
+                page = pq(requests.get(guidestar_url).content)
+            except:
+                time.sleep(60)
+
+        rec = {}
+        for selector, field in classes.items():
+            if not field.endswith('[]'):
+                value = page.find(selector).text().strip().replace('\n', '')
+                if len(value) > 0:
+                    rec[field] = value
+            else:
+                field = field.replace('[]', '')
+                values = page.find(selector)
+                values = [pq(value).text().strip().replace('\n', '') for value in values]
+                values = [value for value in values if len(value) > 0]
+                if len(values) > 0:
+                    rec[field] = ";".join(values)
+            rec.setdefault(field, '')
+        rec['id'] = ass_id
+
+        yield rec
+
+
+def process_resources(res_iter_):
+    first = next(res_iter_)
+    all_ids = (rec['Association_Number'] for rec in first)
+    yield scrape_guidestar(all_ids)
+
+    for res in res_iter_:
+        yield res
+
+
+headers = ["id",
+           "title",
+           "org_kind",
+           "proper_management",
+           "year_established",
+           "address",
+           "objective",
+           "founders"]
+
+resource = datapackage['resources'][0]
+resource.update(parameters)
+resource.setdefault('schema', {})['fields'] = [
+    {'name': header, 'type': 'string'}
+    for header in headers
+]
+
+spew(datapackage, process_resources(res_iter))
