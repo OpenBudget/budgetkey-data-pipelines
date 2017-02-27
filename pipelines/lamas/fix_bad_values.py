3567e7e026552a98af5586df023b2fb63a5383e2
Adam Kariv <adam.kariv@gmail.com>
2017-02-27 21:50:06 +0200

    Complete Lamas Data

--- pipelines/lamas/fix_bad_values.py
+++ pipelines/lamas/fix_bad_values.py
@@ -1,13 +1,34 @@
-from datapackage_pipelines.wrapper import process
+import re
+
+from datapackage_pipelines.wrapper import ingest, spew
+
+parameters, datapackage, res_iter = ingest()
 
 
-def fix_bad_values(row, row_index,
-                   resource_descriptor, resource_index,
-                   parameters, stats):
-    for k, v in row.items():
-        if v in ['..']:
-            row[k] = ''
-    return row
+def process_resource(res_):
+    for row in res_:
+        semel = row.get('symbol_municipality_2015')
+        try:
+            # Make sure this is a valid row
+            int(semel)
+            for k, v in row.items():
+                if v in ['..']:
+                    row[k] = ''
+                if isinstance(v, str) and len(v) > 1:
+                    if v[0] == '(' and v[-1] == ')':
+                        v = row[k] = ''
+                    if '.000' in v:
+                        v = row[k] = v.replace('.000', '')
+                    if len(re.findall('[.]', v)) > 1:
+                        v = row[k] = ''
+
+            yield row
+        except ValueError or TypeError:
+            continue
 
 
-process(process_row=fix_bad_values)
+def process_resources(res_iter_):
+    for res in res_iter_:
+        yield process_resource(res)
+
+spew(datapackage, process_resources(res_iter))
