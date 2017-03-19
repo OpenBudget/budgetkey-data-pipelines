20c11a8c4286ba18fb6e0075f66c281e6a6a5ce3
Ori Hoch <ori@uumpa.com>
2017-03-19 14:27:43 +0200

    fixes to the cli environment

--- common/cli.py
+++ common/cli.py
@@ -1,7 +1,14 @@
-from datapackage_pipelines.cli import cli
 import os
 
 
 def main(*args, **kwargs):
-    os.environ.setdefault("DPP_PROCESSOR_PATH", os.path.join(os.path.dirname(__file__), '..', 'pipelines'))
-    return cli(*args, **kwargs)
+    original_directory = os.getcwd()
+    try:
+        base_path = os.path.join(os.path.dirname(__file__), '..')
+        os.chdir(os.path.join(base_path, 'pipelines'))
+        os.environ.setdefault("DPP_PROCESSOR_PATH", os.path.join(base_path, 'processors'))
+        # so we have to import after setting DPP_PROCESSOR_PATH because this environment variable is read on import
+        from datapackage_pipelines.cli import cli
+        cli(*args, **kwargs)
+    finally:
+        os.chdir(original_directory)
