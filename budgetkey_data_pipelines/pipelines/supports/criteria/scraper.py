e7d809da2e6265406b9c31260b2891eee1cb90f3
Lea <gogogta@gmail.com>
2017-05-15 21:43:23 +0300

    wip

---
+++ budgetkey_data_pipelines/pipelines/supports/criteria/scraper.py
@@ -0,0 +1,69 @@
+from itertools import chain
+
+import requests
+import logging
+from pyquery import PyQuery as pq
+
+from datapackage_pipelines.wrapper import ingest, spew
+
+parameters, datapackage, res_iter = ingest()
+
+URL = "http://www.justice.gov.il/Units/Tmihot/Pages/TestServies.aspx?WPID=WPQ8&PN={0}"
+
+
+def get_all_reports():
+    page = 0
+    session = requests.session()
+    done = False
+    while not done:
+        page = session.get(URL.format(page), verify=False).text
+        page = pq(page)
+        rows = page.find('#cqwpGridViewTable .zbTRBlue, #cqwpGridViewTable .Trtblclss')
+        rows = list(rows)
+        for r in rows:
+            row = pq(r)
+            cells = [pq(td) for td in row.find('td')]
+            rec = {
+                "title": cells[0].text(),
+                "paper_type": cells[1].text(),
+                "office": cells[2].text(),
+                "date": cells[3].text(),
+                "pdf-url": pq(cells[4].find('a')).attr('href').split("'")[-2]
+            }
+            yield rec
+        if len(page) == 0:
+            done = True
+        page += 1
+
+
+resource = parameters['target-resource']
+resource['schema'] = {
+    'fields': [
+        {
+            'name': 'date',
+            'type': 'date',
+            'format': 'fmt:%d-%m-%Y'
+        },
+        {
+            'name': 'title',
+            'type': 'string'
+        },
+        {
+            'name': 'paper_type',
+            'type': 'string'
+        },
+{
+            'name': 'office',
+            'type': 'string'
+        },
+        {
+            'name': 'pdf-url',
+            'type': 'string',
+            'format': 'uri'
+        }
+    ]
+}
+datapackage['resources'].append(resource)
+
+logging.info(datapackage)
+spew(datapackage, chain(res_iter, [get_all_reports()]))
