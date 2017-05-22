d772d295a2f9847235519a23809e14b8373c0565
barakyosi <barakyosi@gmail.com>
2017-05-22 22:15:46 +0300

    Adding ottoman utils test

--- budgetkey_data_pipelines/pipelines/entities/ottoman/fix_values.py
+++ budgetkey_data_pipelines/pipelines/entities/ottoman/fix_values.py
@@ -1,13 +1,4 @@
 from datapackage_pipelines.wrapper import process
-
-
-def process_row(row, *_):
-    if len(row['id']) != 11:
-        return None
-    row['id'] = row['id'][:-2]
-    row['name'] = row['name'].strip()
-    row['address'] = row['address'].strip()
-    return row
-
+from .ottoman_utils import process_row
 
 process(process_row=process_row)
---
+++ tests/pipelines/entities/ottoman/test_ottman_utils.py
@@ -0,0 +1,15 @@
+from budgetkey_data_pipelines.pipelines.entities.ottoman.ottoman_utils import process_row
+
+
+def test_process_row_short_id_return_none():
+    row = {'id': '21'}
+    result = process_row(row)
+    assert result is None
+
+
+def test_process_row_fix_id_strip_name_address():
+    row = {'id': '123456789.0', 'name': '  my name  ', 'address': '  my address  '}
+    result = process_row(row)
+    assert result['id'] == '123456789'
+    assert result['name'] == 'my name'
+    assert result['address'] == 'my address'
