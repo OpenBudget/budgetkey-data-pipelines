9895aea72bd53969fb35c46660e5c424fcbff973
Adam Kariv <adam.kariv@gmail.com>
2017-06-12 19:29:42 +0300

    Add score to spending records

---
+++ budgetkey_data_pipelines/pipelines/procurement/spending/calc-spending-score.py
@@ -0,0 +1,27 @@
+import math
+
+import logging
+from datapackage_pipelines.wrapper import process
+
+
+def modify_datapackage(dp, *_):
+    dp['resources'][0]['schema']['fields'].append({
+        'name': 'score',
+        'type': 'number',
+        'es:score-column': True
+    })
+    return dp
+
+
+def process_row(row, *_):
+    amount = row.get('volume', 0)
+    if amount is None:
+        logging.warning('volume is None: %r', row)
+        amount = 0
+    # year_score = (row['year'] - 1990) / 3.0
+    row['score'] = math.log(max(1, amount)) # + year_score
+    return row
+
+
+process(modify_datapackage=modify_datapackage,
+        process_row=process_row)
