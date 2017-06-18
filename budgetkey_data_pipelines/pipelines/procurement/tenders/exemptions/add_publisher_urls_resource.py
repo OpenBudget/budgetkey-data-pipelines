70cc020e16247e05428c6c964014bb1ba8f86370
Adam Kariv <akariv@users.noreply.github.com>
2017-06-18 21:39:22 +0300

    exemptions: finalize the pipeline, everything should work (#13) (#39)
    * WIP continue exemptions scraper
    
    * finalize scraping of exemption urls
    
    * exemptions: added retry logic and misc. refactoring
    
    * exemptions scraper: added max_pages parameter
    
    * refactor form_data logic to make it clearer
    
    * fix typoe - resources_filter should be resource_filter (singular)
    
    * added download and parse exemption rsources
    
    * [WIP] exemptions downlod / parse exemption data
    
    * minor fix for exemptions page data scraper
    
    * remove the exemption documents processor - it doesn't work properly and not really needed
    
    * modify to a single pipeline, added environment variable to help testing locally, added support for stats
    
    * finalize and stabalize
    
    * added documents resource, test and finalize sql updates
    
    * minor fixes
    
    * change documents to array field, remove related documents table, dynamic update
    
    * minor
    
    * fix problem with textract which introduced unnecessary dependency in latest version

--- budgetkey_data_pipelines/pipelines/procurement/tenders/exemptions/add_publisher_urls_resource.py
+++ budgetkey_data_pipelines/pipelines/procurement/tenders/exemptions/add_publisher_urls_resource.py
@@ -1,5 +1,7 @@
 from budgetkey_data_pipelines.common.resource_filter_processor import ResourceFilterProcessor
 from budgetkey_data_pipelines.pipelines.procurement.tenders.exemptions.exemptions_scraper import ExemptionsPublisherScraper
+import os
+import logging
 
 # TODO: find a better way to have a "mock" processor
 # currently this is the best way I found to do it, it allows to test this processor using the fixture tests
@@ -15,11 +17,24 @@ MOCK_URLS = {10: ['/ExemptionMessage/Pages/ExemptionMessage.aspx?pID=596877',
              45: []}
 
 def resource_filter(resource_data, parameters):
+    max_pages = os.environ.get("EXEMPTION_PUBLISHERS_MAX_PAGES", "")
+    if not max_pages:
+        max_pages = parameters.get("max_pages", "-1")
+    kwargs = {
+        "max_pages": int(max_pages)
+    }
+    limit_publisher_ids = os.environ.get("EXEMPTIONS_LIMIT_PUBLISHER_IDS", None)
+    if limit_publisher_ids:
+        limit_publisher_ids = limit_publisher_ids.split(",")
+    logging.info("limit_publisher_ids={}".format(limit_publisher_ids))
     for publisher in resource_data:
-        scraper = ExemptionsPublisherScraper(publisher["id"])
-        urls = scraper.get_urls() if not parameters.get("mock") else MOCK_URLS[publisher["id"]]
-        for url in urls:
-            yield {"id": publisher["id"], "url": url}
+        publisher_id = publisher["id"]
+        if not limit_publisher_ids or publisher_id in map(int, limit_publisher_ids):
+            logging.info("processing publisher {}".format(publisher_id))
+            scraper = ExemptionsPublisherScraper(publisher_id, **kwargs)
+            urls = scraper.get_urls() if not parameters.get("mock") else MOCK_URLS[publisher_id]
+            for url in urls:
+                yield {"id": publisher_id, "url": url}
 
 ResourceFilterProcessor(default_input_resource="publishers",
                         default_output_resource="publisher-urls",
