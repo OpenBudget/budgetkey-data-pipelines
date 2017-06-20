65dbc9ac8dc2dcf11436a40df0caa30b99d739e2
Adam Kariv <adam.kariv@gmail.com>
2017-06-20 10:33:26 +0300

    Fix tests for exemption scraper

--- tests/procurement/tenders/exemptions/test_download_exemption_pages.py
+++ tests/procurement/tenders/exemptions/test_download_exemption_pages.py
@@ -19,14 +19,6 @@ class MockDownloadExemptionPagesDataProcessor(DownloadExemptionPagesDataProcesso
     def spew(self):
         return self._get_spew_params()
 
-    def _initialize_db_session(self):
-        pass
-
-    def _get_all_existing_ids(self, db_table):
-        if db_table != "procurement_tenders_exemptions":
-            raise Exception()
-        return self.mock_existing_ids
-
     def _get_url_response_text(self, url, timeout):
         if timeout != 180:
             raise Exception("invalid timeout: {}".format(timeout))
@@ -40,8 +32,8 @@ class MockDownloadExemptionPagesDataProcessor(DownloadExemptionPagesDataProcesso
 
 def test():
     resources = [[
-        {"id": 71, "url": "/ExemptionMessage/Pages/ExemptionMessage.aspx?pID=595431"},
-        {"id": 71, "url": "/ExemptionMessage/Pages/ExemptionMessage.aspx?pID=594269"}
+        {"id": 71, "url": "/ExemptionMessage/Pages/ExemptionMessage.aspx?pID=595431", "is_new": True},
+        {"id": 71, "url": "/ExemptionMessage/Pages/ExemptionMessage.aspx?pID=594269", "is_new": False}
     ]]
     ingest_response = ({}, {"resources": [{"name": "publisher-urls"}]}, unlistify_resources(resources))
     datapackage, resources, stats = MockDownloadExemptionPagesDataProcessor(ingest_response=ingest_response).spew()
@@ -58,32 +50,8 @@ def test():
     }]}
     assert listify_resources(resources) == [[
         {"pid": 71, "url": "http://www.mr.gov.il/ExemptionMessage/Pages/ExemptionMessage.aspx?pID=595431", "data": get_mock_exemption_data("595431")},
-        {"pid": 71, "url": "http://www.mr.gov.il/ExemptionMessage/Pages/ExemptionMessage.aspx?pID=594269", "data": get_mock_exemption_data("594269")}
+        # {"pid": 71, "url": "http://www.mr.gov.il/ExemptionMessage/Pages/ExemptionMessage.aspx?pID=594269", "data": get_mock_exemption_data("594269")}
     ]]
     assert stats == {}
 
 
-def test_only_insert():
-    resources = [[
-        {"id": 71, "url": "/ExemptionMessage/Pages/ExemptionMessage.aspx?pID=595431"},
-        {"id": 71, "url": "/ExemptionMessage/Pages/ExemptionMessage.aspx?pID=594269"}
-    ]]
-    ingest_response = ({},
-                       {"resources": [{"name": "publisher-urls"}]}, unlistify_resources(resources))
-    datapackage, resources, stats = MockDownloadExemptionPagesDataProcessor(ingest_response=ingest_response,
-                                                                            mock_existing_ids=["595431"]).spew()
-    # no parameters - both items are spewed
-    assert len([1 for i in next(resources)]) == 2
-    # seting the db-table - only 1 item is spewed, other is in the mock_existing_ids
-    ingest_response = ({"db-table": "procurement_tenders_exemptions"},
-                       {"resources": [{"name": "publisher-urls"}]}, unlistify_resources(resources))
-    datapackage, resources, stats = MockDownloadExemptionPagesDataProcessor(ingest_response=ingest_response,
-                                                                            mock_existing_ids=["595431"]).spew()
-    for i, resource in enumerate(listify_resources(resources)):
-        for j, row in enumerate(resource):
-            if i == 0 and j == 0:
-                assert row["pid"] == 71
-                assert row["url"] == "http://www.mr.gov.il/ExemptionMessage/Pages/ExemptionMessage.aspx?pID=594269"
-            else:
-                raise Exception()
-    assert stats == {}
