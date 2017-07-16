8bb73151929c8ffb3d5395fd0e8e2a9d15e69051
Adam Kariv <adam.kariv@gmail.com>
2017-07-16 11:53:14 +0300

    Change the way collate stores schemas

--- budgetkey_data_pipelines/processors/collate_schema.py
+++ budgetkey_data_pipelines/processors/collate_schema.py
@@ -2,18 +2,26 @@ from datapackage_pipelines.wrapper import process
 
 
 def modify_datapackage(dp, *_):
+
+    schemas = {}
+    to_del = []
+    for k, v in dp.items():
+        if k.startswith('collated-schema:'):
+            _, res_name, collated_field = k.split(':')
+            schemas.setdefault(res_name, {})[collated_field] = v
+            to_del.append(k)
+    for k in to_del:
+        del dp[k]
+
     for res in dp['resources']:
-        for k, v in res.items():
-            if k.startswith('collated-schema:'):
-                collated_field = k[len('collated-schema:'):]
-                for f in res['schema']['fields']:
-                    if f['name'] == collated_field:
-                        assert f['type'] == 'array'
-                        f['es:schema'] = v
-                        f['es:itemType'] = 'object'
-                        break
-                del res[k]
-                break
+        if res['name'] not in schemas:
+            continue
+        res_schemas = schemas[res['name']]
+        for f in res['schema']['fields']:
+            if f['name'] in res_schemas:
+                assert f['type'] == 'array'
+                f['es:schema'] = res_schemas[f['name']]
+                f['es:itemType'] = 'object'
     return dp
 
 process(modify_datapackage=modify_datapackage)
