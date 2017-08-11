079995a2976b24d298ac9f44d4d74002c71d4d72
Simon <simonbor@gmail.com>
2017-08-11 09:34:52 +0300

    general flow is ready

--- budgetkey_data_pipelines/pipelines/entities/moital/scraper.py
+++ budgetkey_data_pipelines/pipelines/entities/moital/scraper.py
@@ -1,6 +1,7 @@
 import time
 import requests
 import logging
+
 from pyquery import PyQuery as pq
 from datapackage_pipelines.wrapper import ingest, spew
 
@@ -17,7 +18,10 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 parameters, datapackage, res_iter = ingest()
 
-host = parameters['url']
+url = parameters['url']
+contractors = parameters['contractors_csv'].split(',')
+extention = parameters['page_extention']
+
 delay = parameters['delay']
 headers = ['close_corporation', 'name', 'id', 'license_number', 'company_owner', 'company_ceo', 
   'address', 'city', 'expired', 'status']
@@ -25,7 +29,7 @@ headers = ['close_corporation', 'name', 'id', 'license_number', 'company_owner',
 def nextPageElement(page):
     return page.find('#gvContractors table[border="0"] span').parent().next()
 
-def scrape(host_):
+def scrape():
 
     driver = webdriver.Remote(
         command_executor='http://127.0.0.1:4444/wd/hub',
@@ -33,33 +37,37 @@ def scrape(host_):
 
     driver.set_window_size(1200, 800)
 
-    def prepare():
-        logging.info('PREPARING')
-        driver.get(host_)
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "gvContractors"))
-        )
+    for contractor in contractors:
+        host_ = url + contractor.strip() + extention
+        logging.info('Working on the host %s', host_)
 
-    prepare()
+        def prepare():
+            logging.info('PREPARING')
+            driver.get(host_)
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.ID, "gvContractors"))
+            )
 
-    while True:
-        page = pq(driver.page_source)
-        rows = page.find('#gvContractors tr[align="right"]')
-        logging.info('Got %d rows', len(rows))
+        prepare()
 
-        for row_ in rows:
-            row = ([" ".join(pq(x).text().strip().split()) for x in pq(row_).find('td')])
-            datum = dict(zip(headers, row))
-            yield datum
+        while True:
+            page = pq(driver.page_source)
+            rows = page.find('#gvContractors tr[align="right"]')
+            logging.info('Got %d rows', len(rows))
 
-        nextElem = nextPageElement(page)
-        if len(nextElem) > 0:
-            logging.info('Navigate to page #%s',nextElem.find('a').text())
-            next_page = driver.find_element_by_xpath("//*[@border=0]/tbody/tr/td/span/parent::td/following-sibling::td/a")
-            next_page.click()
-            time.sleep(delay)
-        else:
-            break
+            for row_ in rows:
+                row = ([" ".join(pq(x).text().strip().split()) for x in pq(row_).find('td')])
+                datum = dict(zip(headers, row))
+                yield datum
+
+            nextElem = nextPageElement(page)
+            if len(nextElem) > 0:
+                logging.info('Navigate to page #%s',nextElem.find('a').text())
+                next_page = driver.find_element_by_xpath("//*[@border=0]/tbody/tr/td/span/parent::td/following-sibling::td/a")
+                next_page.click()
+                time.sleep(delay)
+            else:
+                break
 
 datapackage['resources'].append({
     'name': 'name',
@@ -71,7 +79,7 @@ datapackage['resources'].append({
     }
 })
 
-spew(datapackage, [scrape(host)])
+spew(datapackage, [scrape()])
 
 # for run the pipeline
 # budgetkey-dpp run ./entities/moital/moital_service_providers
