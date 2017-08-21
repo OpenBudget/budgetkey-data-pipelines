85ef5e7435379722a502763a3b6df76ec3bb447d
Ori Hoch <ori@uumpa.com>
2017-08-21 14:55:48 +0300

    misc. optimizations to tenders processors

---
+++ budgetkey_data_pipelines/processors/throttle.py
@@ -0,0 +1,38 @@
+from datapackage_pipelines.wrapper import spew, ingest
+import time, logging, datetime
+
+
+def filter_resource(resource, sleep_seconds, start_time, log_interval_seconds):
+    last_log_seconds = None
+    num_processed_rows = 0
+    for row in resource:
+        yield row
+        num_processed_rows += 1
+        elapsed_seconds = (datetime.datetime.now()-start_time).total_seconds()
+        if not last_log_seconds:
+            seconds_since_last_log = elapsed_seconds
+        else:
+            seconds_since_last_log = elapsed_seconds - last_log_seconds
+        if seconds_since_last_log > log_interval_seconds:
+            last_log_seconds = elapsed_seconds
+            logging.info("processed {} rows, elapsed time (seconds)={}".format(num_processed_rows,
+                                                                               elapsed_seconds))
+        time.sleep(sleep_seconds)
+
+
+def filter_resources(datapackage, resources, parameters):
+    input_resource_name = parameters.get("resource")
+    sleep_seconds = float(parameters.get("sleep-seconds", 2))  # sleep 2 seconds between rows
+    log_interval_seconds = int(parameters.get("log-interval-seconds", 60))  # log every 60 seconds
+    start_time = datetime.datetime.now()
+    for resource_descriptor, resource in zip(datapackage["resources"], resources):
+        if not input_resource_name or input_resource_name == resource_descriptor["name"]:
+            logging.info("throttling resource {}: sleep_seconds={}".format(resource_descriptor["name"],
+                                                                           sleep_seconds))
+            yield filter_resource(resource, sleep_seconds, start_time, log_interval_seconds)
+        else:
+            yield resource
+
+
+parameters, datapackage, resources = ingest()
+spew(datapackage, filter_resources(datapackage, resources, parameters))
