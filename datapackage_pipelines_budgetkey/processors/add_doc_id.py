6a4a08fc713c8a655d7cf84f5ee7e3770b52cf48
Adam Kariv <adam.kariv@gmail.com>
2017-10-09 18:22:29 +0300

    Fix typo

--- datapackage_pipelines_budgetkey/processors/add_doc_id.py
+++ datapackage_pipelines_budgetkey/processors/add_doc_id.py
@@ -8,7 +8,7 @@ key_pattern = parameters['doc-id-pattern']
 def process_resources(res_iter_):
     for res in res_iter_:
         for row in res:
-            row['doc_id'] = key_pattern.format(**row),
+            row['doc_id'] = key_pattern.format(**row)
             yield row
 
 dp['resources'][0]['schema']['fields'].append(
