2067309eac3c15213a03f530658a5c762b9936d7
Adam Kariv <adam.kariv@gmail.com>
2017-11-08 10:49:14 +0200

    Change gov company collated field name

--- datapackage_pipelines_budgetkey/pipelines/entities/companies/registrar/fix_values.py
+++ datapackage_pipelines_budgetkey/pipelines/entities/companies/registrar/fix_values.py
@@ -1,24 +1,25 @@
 from datapackage_pipelines.wrapper import process
 
+
 def process_row(row, *_):
-    row['company_address'] = []
-    if row.get('company_address') and \
+    row['company_address_lines'] = []
+    if row.get('company_address_lines') and \
             row.get('company_street_number') and \
             row.get('company_city'):
-        row['company_address'].append('{company_street} {company_street_number},'.format(**row))
+        row['company_address_lines'].append('{company_street} {company_street_number},'.format(**row))
     elif row.get('company_pob') and \
             row.get('company_pob_city') and \
             row.get('company_pob_postal_code'):
-        row['company_address'].append('ת.ד.' + '{company_pob} {company_pob_city} {company_pob_postal_code}'.format(**row))
+        row['company_address_lines'].append('ת.ד.' + '{company_pob} {company_pob_city} {company_pob_postal_code}'.format(**row))
     if row.get('company_located_at'):
-        row['company_address'].append('אצל: ' + '{company_located_at}'.format(**row))
+        row['company_address_lines'].append('אצל: ' + '{company_located_at}'.format(**row))
 
     return row
 
 
 def modify_datapackage(dp, *_):
     dp['resources'][0]['schema']['fields'].append({
-        'name': 'company_address',
+        'name': 'company_address_lines',
         'type': 'array',
         'es:itemType': 'string'
     })
