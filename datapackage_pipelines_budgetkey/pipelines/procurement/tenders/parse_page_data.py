ce44c653af6d6ae2478f52018ad91244c557981e
Adam Kariv <adam.kariv@gmail.com>
2017-11-28 09:48:51 +0200

    Avoid unsigning too many documents in one batch

--- datapackage_pipelines_budgetkey/pipelines/procurement/tenders/parse_page_data.py
+++ datapackage_pipelines_budgetkey/pipelines/procurement/tenders/parse_page_data.py
@@ -89,6 +89,7 @@ class ParsePageDataProcessor(ResourceFilterProcessor):
                                                      table_schema=TABLE_SCHEMA,
                                                      **kwargs)
         self.base_object_name = "procurement/tenders/"
+        self.unsigned = 0
 
     def requests_get_content(self, url):
         return requests.get(url).content
@@ -144,7 +145,11 @@ class ParsePageDataProcessor(ResourceFilterProcessor):
         source_url = "{}{}".format(base_url, href)
         if source_url.endswith(".signed"):
             # signed documents - decrypt and change path to local unsigned file
-            return self.unsign_document_link(source_url)
+            self.unsigned += 1
+            if self.unsigned < 500:
+                return self.unsign_document_link(source_url)
+            else:
+                return None
         else:
             # unsigned documents are returned with the source url as-is
             return source_url
