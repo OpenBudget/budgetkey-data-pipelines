2bcd8a244fd148ecc0669cbf9b0786c6c1d2b1d3
Adam Kariv <adam.kariv@gmail.com>
2018-02-08 19:00:25 +0200

    Adding association activity districts

--- datapackage_pipelines_budgetkey/pipelines/entities/associations/guidestar/fix_values.py
+++ datapackage_pipelines_budgetkey/pipelines/entities/associations/guidestar/fix_values.py
@@ -1,4 +1,14 @@
 from datapackage_pipelines.wrapper import process
+from datapackage import Package
+
+from fuzzywuzzy import process as fw_process
+
+lamas_data = Package('/var/datapackages/lamas-municipal-data/datapackage.json')
+
+districts = dict(
+    (x['entity_name'], x['district_2015'])
+    for x in lamas_data.resources[0].iter(keyed=True)
+)
 
 
 def process_row(row, *_):
@@ -7,6 +17,12 @@ def process_row(row, *_):
         row['association_activity_region_list'] = [x.strip() for x in association_activity_region.split(',')]
     else:
         row['association_activity_region_list'] = []
+    association_activity_region_districts = set()
+    for city in row['association_activity_region_list']:
+        best = fw_process.extract(city, districts.keys())
+        association_activity_region_districts.add(best)
+    row['association_activity_region_districts'] = list(association_activity_region_districts)
+
     return row
 
 
@@ -14,6 +30,11 @@ def modify_datapackage(dp, *_):
     dp['resources'][0]['schema']['fields'].extend([
         {
             'name': 'association_activity_region_list',
+            'type': 'array',
+            'es:itemType': 'string'
+        },
+        {
+            'name': 'association_activity_region_districts',
             'type': 'array',
             'es:itemType': 'string'
         }
