667bab1b18033632b2645c38457bae017ab7261f
Adam Kariv <adam.kariv@gmail.com>
2018-03-11 19:32:50 +0200

    Fix primary key for latest spending

--- datapackage_pipelines_budgetkey/processors/throttle_resources.py
+++ datapackage_pipelines_budgetkey/processors/throttle_resources.py
@@ -2,22 +2,16 @@ from datapackage_pipelines.wrapper import spew, ingest
 import time, logging, datetime, sys
 
 
-def filter_resource(resource, sleep_seconds):
-    yield from resource
-    time.sleep(sleep_seconds)
-
-
-def filter_resources(datapackage, resources, parameters):
+def filter_resources(resources, parameters):
     input_resource_name = parameters.get("resource")
     sleep_seconds = float(parameters.get("sleep-seconds", 2))  # sleep 2 seconds between resources
-    for resource_descriptor, resource in zip(datapackage["resources"], resources):
-        if not input_resource_name or input_resource_name == resource_descriptor["name"]:
-            logging.info("throttling resource {}: sleep_seconds={}".format(resource_descriptor["name"],
+    for resource in resources:
+        yield resource
+        if not input_resource_name or input_resource_name == resource.spec["name"]:
+            logging.info("throttling resource {}: sleep_seconds={}".format(resource.spec["name"],
                                                                            sleep_seconds))
-            yield filter_resource(resource, sleep_seconds)
-        else:
-            yield resource
+            time.sleep(sleep_seconds)
 
 
 parameters, datapackage, resources = ingest()
-spew(datapackage, filter_resources(datapackage, resources, parameters))
+spew(datapackage, filter_resources(resources, parameters))
