8c7e3a4f71c1c083d3c9e645cd6718cf4781394f
Adam Kariv <adam.kariv@gmail.com>
2018-03-13 21:25:48 +0200

    Further improve performance by boosting fingerprinting

--- datapackage_pipelines_budgetkey/processors/fingerprint.py
+++ datapackage_pipelines_budgetkey/processors/fingerprint.py
@@ -1,4 +1,7 @@
 import re
+import os
+
+from sqlalchemy import create_engine
 
 from datapackage_pipelines.wrapper import ingest, spew
 
@@ -65,68 +68,88 @@ for w in CLEAN_WORDS:
     for i in range(1, len(w)+1):
         CLEAN_SUFFIXES.add(" "+w[:i])
 
+def calc_fingerprint(name):
+    tgt = name
+    options = []
+    if tgt is not None:
+        tgt = name.strip().lower()
+        options.append(tgt)
 
-def fingerprint(rows, src_field, tgt_field, unique_fingerprints):
+        done = False
+        while not done:
+            done = True
+            for suffix in CLEAN_SUFFIXES:
+                for l in range(len(suffix), 0, -1):
+                    if tgt.endswith(suffix[:l]):
+                        tgt = tgt[:-l]
+                        options.append(tgt)
+                        done = False
+                        break
+
+        for prefix in CLEAN_TITLES:
+            if tgt.startswith(prefix + ' '):
+                tgt = tgt[len(prefix)+1:]
+                options.append(tgt)
+
+        for f, t in SIMPLIFICATIONS:
+            tgt = tgt.replace(f, t)
+
+        options.append(tgt)
+
+        tgt = DIGITS.sub('', tgt)
+        options.append(tgt)
+
+        tgt = ENGLISH.sub('', tgt)
+        options.append(tgt)
+
+        if not tgt:
+            tgt = options[0]
+            for opt in reversed(options):
+                if opt:
+                    tgt = opt.strip()
+                    break
+        
+        if tgt is not None:
+            tgt = ' '.join(sorted(tgt[:30].split()))
+
+    if not tgt:
+        tgt = '<empty>'
+        
+    return tgt
+
+
+def fingerprint(rows, src_field, tgt_field, src_id_field, unique_fingerprints):
     used = set()
     for row in rows:
         name = row[src_field]
-        tgt = name
-        options = []
-        if name is not None:
-            tgt = name.strip().lower()
-            options.append(tgt)
-
-            done = False
-            while not done:
-                done = True
-                for suffix in CLEAN_SUFFIXES:
-                    for l in range(len(suffix), 0, -1):
-                        if tgt.endswith(suffix[:l]):
-                            tgt = tgt[:-l]
-                            options.append(tgt)
-                            done = False
-                            break
-
-            for prefix in CLEAN_TITLES:
-                if tgt.startswith(prefix + ' '):
-                    tgt = tgt[len(prefix)+1:]
-                    options.append(tgt)
-
-            for f, t in SIMPLIFICATIONS:
-                tgt = tgt.replace(f, t)
-
-            options.append(tgt)
-
-            tgt = DIGITS.sub('', tgt)
-            options.append(tgt)
-
-            tgt = ENGLISH.sub('', tgt)
-            options.append(tgt)
-
-            if not tgt:
-                tgt = options[0]
-                for opt in reversed(options):
-                    if opt:
-                        tgt = opt.strip()
-                        break
-            
-            if tgt is not None:
-                tgt = ' '.join(sorted(tgt[:30].split()))
-
-        if not tgt:
-            tgt = '<empty>'
-            
-        if not unique_fingerprints or tgt not in used:
-            row[tgt_field] = tgt
+        if tgt_field:
+            tgt = calc_fingerprint(name)
+            if not unique_fingerprints or tgt not in used:
+                row[tgt_field] = tgt
+                yield row
+                if unique_fingerprints:
+                    used.add(tgt)
+        else:
+            engine = create_engine(os.environ['DPP_DB_ENGINE'])
+            query = "select id, name, kind from entity_fingerprints where {}='{}' limit 1"
+            if src_id_field and row[src_id_field]:
+                results = engine.execute(query.format('id', row[src_id_field])).fetchall()
+            res = None
+            if len(results) > 0:
+                res = results[0]
+            else:
+                results = engine.execute(query.format('fingerprint', calc_fingerprint(row[src_field]))).fetchall()
+                if len(results) > 0:
+                    res = results[0]
+            if res is not None:
+                row['entity_id'], row['entity_name'], row['entity_kind'] = res[0], res[1], res[2]
             yield row
-            if unique_fingerprints:
-                used.add(tgt)
 
 
-def process_resources(res_iter_, src_field, tgt_field, unique_fingerprints):
+def process_resources(res_iter_, src_field, tgt_field, src_id_field, unique_fingerprints):
     for res in res_iter_:
         if res.spec['name'] == res_name:
-            yield fingerprint(res, src_field, tgt_field, unique_fingerprints)
+            yield fingerprint(res, src_field, tgt_field, src_id_field, unique_fingerprints)
         else:
             yield res
 
@@ -134,7 +157,9 @@ def process_resources(res_iter_, src_field, tgt_field, unique_fingerprints):
 if __name__ == "__main__":
     parameters, dp, res_iter = ingest()
     src_field = parameters['source-field']
-    tgt_field = parameters['target-field']
+    src_id_field = parameters.get('source-id-field')
+    tgt_field = parameters.get('target-field')
+
     res_name = parameters['resource-name']
     unique_fingerprints = parameters.get('unique-fingerprints', False)
 
@@ -147,4 +172,4 @@ if __name__ == "__main__":
         'type': 'string'
     })
 
-    spew(dp, process_resources(res_iter, src_field, tgt_field, unique_fingerprints))
+    spew(dp, process_resources(res_iter, src_field, tgt_field, src_id_field, unique_fingerprints))
