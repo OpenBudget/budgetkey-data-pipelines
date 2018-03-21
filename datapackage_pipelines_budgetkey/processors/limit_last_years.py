b3ef24d4c50ef6e53eeec05280c611864aed3ca0
Adam Kariv <adam.kariv@gmail.com>
2018-03-21 14:13:32 +0200

    Add latest incomes for entities

---
+++ datapackage_pipelines_budgetkey/processors/limit_last_years.py
@@ -0,0 +1,29 @@
+import datetime
+
+from datapackage_pipelines.wrapper import process
+
+min_year = (datetime.datetime.now() - datetime.timedelta(weeks=26)).year - 3
+
+def process_row(row, row_index,
+                spec, resource_index,
+                parameters, stats):
+    year = row[parameters['year_column']]
+    target_col = parameters['target_column']
+    if year is not None and year >= min_year:
+        row[target_col] = row[parameters['amount_column']]
+    else:
+        row[target_col] = 0
+    return row
+
+
+def modify_datapackage(datapackage, parameters, _):
+    datapackage['resources'][0]['schema']['fields'].append({
+        'name': parameters['target_column'],
+        'type': 'number'
+    })
+    return datapackage
+
+
+if __name__=="__main__":
+    process(modify_datapackage=modify_datapackage,
+            process_row=process_row)
