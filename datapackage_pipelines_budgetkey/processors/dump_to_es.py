f87680109d4b27e46cb0dd01a492ccc921de1b85
Adam Kariv <adam.kariv@gmail.com>
2018-03-23 21:36:57 +0300

    debugging

--- datapackage_pipelines_budgetkey/processors/dump_to_es.py
+++ datapackage_pipelines_budgetkey/processors/dump_to_es.py
@@ -1,6 +1,9 @@
 from datapackage_pipelines_elasticsearch.processors.dump.to_index import ESDumper
 from tableschema_elasticsearch.mappers import MappingGenerator
 
+import logging
+import collections
+
 
 class BoostingMappingGenerator(MappingGenerator):
 
@@ -32,6 +35,25 @@ class DumpToElasticSearch(ESDumper):
                 'index.mapping.coerce': True
             }
         )
+
+    def handle_resource(self, resource, spec, parameters, datapackage):
+        def debugger(resource):
+            resource_name = spec['name']
+            seen = 0
+            days_update = collections.Counter()
+            debugged = None
+            for row in resource:
+                if row.get('key') == 'org/association/520028275' or row.get('id') == '520028275':
+                    debugged = row
+                    logging.error('%s: debug %r', resource_name, row)
+                seen += 1
+                days_update.update([row.get('__next_update_days')])
+                yield row
+            logging.error('%s: debug %r', resource_name, debugged)
+            logging.error('%s: __next_update_days %r', resource_name, days_update.most_common())
+            logging.error('%s: indexed %d rows', resource_name, seen)
+        
+        return super(DumpToElasticSearch, self).handle_resource(debugger(resource), spec, parameters, datapackage)
 
 
 DumpToElasticSearch()()
