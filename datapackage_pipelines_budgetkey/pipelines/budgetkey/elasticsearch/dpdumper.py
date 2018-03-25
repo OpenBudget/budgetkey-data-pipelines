8eb541226cc2fb03d86086d5449b2cd2f3b3459e
Ori Hoch <ori@uumpa.com>
2018-03-25 15:47:02 +0300

    dpdumper: create directory before writing the datapackage.json

--- datapackage_pipelines_budgetkey/pipelines/budgetkey/elasticsearch/dpdumper.py
+++ datapackage_pipelines_budgetkey/pipelines/budgetkey/elasticsearch/dpdumper.py
@@ -4,6 +4,7 @@ import json
 from datapackage_pipelines.wrapper import process
 
 def modify_datapackage(dp, parameters, _):
+    os.makedirs(parameters['out-path'], exist_ok=True)
     json.dump(dp, open(os.path.join(parameters['out-path'], 'datapackage.json'), 'w'))
     return dp
 
