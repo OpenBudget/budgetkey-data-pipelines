dc4b974cae6f9bee49d83d588748fb3998fcae84
Adam Kariv <adam.kariv@gmail.com>
2018-04-08 12:14:52 +0300

    Add total income calculation to activity reports

--- datapackage_pipelines_budgetkey/pipelines/budgetkey/elasticsearch/make_ngo_report_income_lists.py
+++ datapackage_pipelines_budgetkey/pipelines/budgetkey/elasticsearch/make_ngo_report_income_lists.py
@@ -14,13 +14,22 @@ engine = create_engine(os.environ['DPP_DB_ENGINE'])
 
 INCOME_SUFFIXES = ['', '_contracts', '_supports']
 INCOME_LIST_QUERY = '''
-SELECT 'org/' || kind || '/' || id as doc_id, name, received_amount{suffix} 
-FROM _elasticsearch_mirror__entities
+SELECT 'org/' || kind || '/' || id as doc_id, name, received_amount{suffix} as amount
+FROM entities_processed
 JOIN guidestar_processed
 USING (id)
 WHERE association_field_of_activity='{foa}'
-and  received_amount{suffix}>0
+AND received_amount{suffix}>0
 ORDER BY 3 desc
+'''
+
+TOTAL_INCOME_LIST_QUERY = '''
+SELECT sum(received_amount{suffix}) as amount
+FROM entities_processed
+JOIN guidestar_processed
+USING (id)
+WHERE association_field_of_activity='{foa}'
+AND received_amount{suffix}>0
 '''
 
 
@@ -40,6 +49,21 @@ def make_income_list(foa, suffix):
         logging.error('Failed to query DB for incomes')
     return result
 
+def make_income_total(foa, suffix):
+    query = TOTAL_INCOME_LIST_QUERY.format(
+        foa=foa, suffix=suffix
+    )
+    result = []
+    try:
+        result = engine.execute(query)
+        result = list(dict(r) for r in result)[0]['amount']
+        if not result:
+            result = 0
+        return float(result)
+    except ProgrammingError:
+        logging.error('Failed to query DB for incomes')
+    return result
+
 def process_resource(res_):
     for row in res_:
         details = row['details']
@@ -47,6 +71,9 @@ def process_resource(res_):
             foa = details['field_of_activity']
             for suffix in INCOME_SUFFIXES:
                 details['income_list{}'.format(suffix)] = make_income_list(
+                    foa, suffix
+                )
+                details['income_total{}'.format(suffix)] = make_income_total(
                     foa, suffix
                 )
         row['details'] = details
@@ -60,14 +87,19 @@ def process_resources(res_iter_):
 
 
 for suffix in INCOME_SUFFIXES:
-    for prefix in ['income_list', 'income_total']:
-        dp['resources'][0]['schema']['fields'].append(
-            {
-                'name': '{}{}'.format(prefix, suffix),
-                'type': 'array',
-                'es:itemType': 'object',
-                'es:index': False
-            },
-        )
+    dp['resources'][0]['schema']['fields'].append(
+        {
+            'name': 'income_list{}'.format(suffix),
+            'type': 'array',
+            'es:itemType': 'object',
+            'es:index': False
+        },
+    )
+    dp['resources'][0]['schema']['fields'].append(
+        {
+            'name': 'income_total{}'.format(suffix),
+            'type': 'number',
+        },
+    )
 
 spew(dp, process_resources(res_iter))
