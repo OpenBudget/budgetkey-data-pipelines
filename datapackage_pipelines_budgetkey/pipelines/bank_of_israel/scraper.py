f42d28f2beb2e063fd37223f66a4709156ada8ea
Assaf Leshem <asfuna@gmail.com>
2018-05-14 22:57:23 +0300

    Bank of Isreal: now scrapes all reports

--- datapackage_pipelines_budgetkey/pipelines/bank_of_israel/scraper.py
+++ datapackage_pipelines_budgetkey/pipelines/bank_of_israel/scraper.py
@@ -1,4 +1,6 @@
 # coding=utf-8
+import hashlib
+
 import requests
 from itertools import chain
 
@@ -9,21 +11,27 @@ from datapackage_pipelines.wrapper import ingest, spew
 
 parameters, datapackage, res_iter = ingest()
 
-SEARCH_PAGE_RESULTS_URL = "http://www.boi.org.il/he/DataAndStatistics/Pages/SeriesSearchBySubject.aspx?Level=4&sId=16"
-REPORT_URL = "http://www.boi.org.il/he/DataAndStatistics/Pages/SeriesData.aspx?SeriesCode={serie_code}&DateStart=01/01/1900&DateEnd=01/01/2100&Level=4&Sid=16"
+sids = range(32)
+SEARCH_PAGE_RESULTS_URL = "http://www.boi.org.il/he/DataAndStatistics/_layouts/boi/handlers/WebPartHandler.aspx?wp" \
+                          "=SeriesSearchBySubject&lang=he-IL&PaggingSize=100&Level=4&sId={sid} "
+REPORT_URL = "http://www.boi.org.il/he/DataAndStatistics/Pages/SeriesData.aspx?SeriesCode={" \
+             "serie_code}&DateStart=01/01/1900&DateEnd=01/01/2100&Level=4&Sid={sid} "
 
-def get_serie_codes():
+
+def get_series(sid):
     session = requests.Session()
-    response = session.get(SEARCH_PAGE_RESULTS_URL)
+    response = session.get(SEARCH_PAGE_RESULTS_URL.format(sid=sid))
     data = response.content
     resp = pq(data.decode(response.encoding))
-    return [serie_code_title.getparent().text_content().split(" ")[-1]
-            for serie_code_title in resp.find(u"table tr b:contains('קוד הסדרה:')")]
+    results = []
+    for serie_code in resp.find("table tr input.hidSeriesCode"):
+        results.append((sid, serie_code.attrib['value'], serie_code.getparent().find("input[@class=\"hidOutpuFreq\"]").attrib['value']))
+    return results
 
 
-def get_report(serie_code):
+def get_report(sid, serie_code):
     session = requests.Session()
-    response = session.get(REPORT_URL.format(serie_code=serie_code))
+    response = session.get(REPORT_URL.format(sid=sid, serie_code=serie_code))
     data = response.content
     resp = pq(data.decode(response.encoding))
     rows = resp.find("table tr")
@@ -40,21 +48,34 @@ def get_report(serie_code):
     return {'metadata': metadata, 'values': values}
 
 
-def get_all_reports():
-    serie_codes = get_serie_codes()
-    for serie_code in serie_codes:
-        report = get_report(serie_code)
-        if report['metadata'][u'תדירות'] == u'חודשית (M)':
+def get_all_reports(series):
+    for sid, code, freq in series:
+        report = get_report(sid, code)
+        yield [{
+                "description": report['metadata'][u'תיאור'],
+                "units": report['metadata'][u'יחידות'],
+                "season_adjusted": report['metadata'][u'ניכוי עונתיות'],
+                "date": value['date'],
+                "value": value['value']
+            } for value in report['values']]
+
+
+def add_series_to_resources(series):
+    for sid, code, freq in series:
+        hexdigest = hashlib.md5(code.encode('utf-8')).hexdigest()
+        if freq == u'חודשית (M)':
             date_format = '%m/%Y'
-        elif report['metadata'][u'תדירות'] == u'רבעונית (Q)':
+        elif freq == u'רבעונית (Q)':
+            date_format = '%d/%m/%Y'
+        elif freq == u'שנתית (A)':
             date_format = '%d/%m/%Y'
         else:
             # TODO: log the frequency so it can be fixed
             continue
         resource = {
             PROP_STREAMING: True,
-            'name': report['metadata'][u'תיאור'],
-            'path': 'data/bank_of_israel/' + str(hash(report['metadata'][u'תיאור'])) + '.csv',
+            'name': hexdigest,
+            'path': 'data/bank_of_israel/' + hexdigest + '.csv',
             'schema': {
                 'fields': [
 
@@ -83,16 +104,6 @@ def get_all_reports():
             }
         }
         datapackage['resources'].append(resource)
-        for value in report['values']:
-            rec = {
-                "description": report['metadata'][u'תיאור'],
-                "units": report['metadata'][u'יחידות'],
-                "season_adjusted": report['metadata'][u'ניכוי עונתיות'],
-                "date": value['date'],
-                "value": value['value']
-            }
-            yield rec
-
 
 # resource = parameters['target-resource']
 # resource[PROP_STREAMING] = True
@@ -125,4 +136,8 @@ def get_all_reports():
 # }
 # datapackage['resources'].append(resource)
 
-spew(datapackage, chain(res_iter, [get_all_reports()]))
+
+series = sum([get_series(sid) for sid in sids], [])
+add_series_to_resources(series)
+reports = get_all_reports(series)
+spew(datapackage, chain(res_iter, reports))
