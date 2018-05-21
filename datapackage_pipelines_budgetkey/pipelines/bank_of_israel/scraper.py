97da1ac00c51aa0ec2df8d49379896f1b592e235
Assaf Leshem <asfuna@gmail.com>
2018-05-21 21:36:19 +0300

    Bank of Israel: refactored for pull request. Added title.

--- datapackage_pipelines_budgetkey/pipelines/bank_of_israel/scraper.py
+++ datapackage_pipelines_budgetkey/pipelines/bank_of_israel/scraper.py
@@ -11,11 +11,22 @@ from datapackage_pipelines.wrapper import ingest, spew
 
 parameters, datapackage, res_iter = ingest()
 
-sids = range(32)
 SEARCH_PAGE_RESULTS_URL = "http://www.boi.org.il/he/DataAndStatistics/_layouts/boi/handlers/WebPartHandler.aspx?wp" \
-                          "=SeriesSearchBySubject&lang=he-IL&PaggingSize=100&Level=4&sId={sid} "
+                          "=SeriesSearchBySubject&lang=he-IL&PaggingSize=100&Level=4&sId={sid}"
 REPORT_URL = "http://www.boi.org.il/he/DataAndStatistics/Pages/SeriesData.aspx?SeriesCode={" \
-             "serie_code}&DateStart=01/01/1900&DateEnd=01/01/2100&Level=4&Sid={sid} "
+             "serie_code}&DateStart=01/01/1900&DateEnd=01/01/2100&Level=4&Sid={sid}"
+
+
+def get_all_series():
+    sid = 1
+    all_series = []
+    series = get_series(sid)
+    while series:
+        all_series += series
+        sid += 1
+        series = get_series(sid)
+
+    return all_series
 
 
 def get_series(sid):
@@ -25,15 +36,18 @@ def get_series(sid):
     resp = pq(data.decode(response.encoding))
     results = []
     for serie_code in resp.find("table tr input.hidSeriesCode"):
-        results.append((sid, serie_code.attrib['value'], serie_code.getparent().find("input[@class=\"hidOutpuFreq\"]").attrib['value']))
+        results.append({'sid': sid, 'code': serie_code.attrib['value'],
+                        'frequency': serie_code.getparent().find("input[@class=\"hidOutpuFreq\"]").attrib['value'],
+                        'title': serie_code.getparent().getparent().getparent().getprevious().getprevious()
+                       .find('td').text_content()})
     return results
 
 
 def get_report(sid, serie_code):
     session = requests.Session()
     response = session.get(REPORT_URL.format(sid=sid, serie_code=serie_code))
-    data = response.content
-    resp = pq(data.decode(response.encoding))
+    data = response.text
+    resp = pq(data)
     rows = resp.find("table tr")
     metadata = {}
     last_metadata_row_index = -1
@@ -49,8 +63,8 @@ def get_report(sid, serie_code):
 
 
 def get_all_reports(series):
-    for sid, code, freq in series:
-        report = get_report(sid, code)
+    for serie in series:
+        report = get_report(serie['sid'], serie['code'])
         yield [{
                 "description": report['metadata'][u'תיאור'],
                 "units": report['metadata'][u'יחידות'],
@@ -61,13 +75,13 @@ def get_all_reports(series):
 
 
 def add_series_to_resources(series):
-    for sid, code, freq in series:
-        hexdigest = hashlib.md5(code.encode('utf-8')).hexdigest()
-        if freq == u'חודשית (M)':
+    for serie in series:
+        hexdigest = hashlib.md5(serie['code'].encode('utf-8')).hexdigest()
+        if serie['frequency'] == u'חודשית (M)':
             date_format = '%m/%Y'
-        elif freq == u'רבעונית (Q)':
+        elif serie['frequency'] == u'רבעונית (Q)':
             date_format = '%d/%m/%Y'
-        elif freq == u'שנתית (A)':
+        elif serie['frequency'] == u'שנתית (A)':
             date_format = '%d/%m/%Y'
         else:
             # TODO: log the frequency so it can be fixed
@@ -75,6 +89,7 @@ def add_series_to_resources(series):
         resource = {
             PROP_STREAMING: True,
             'name': hexdigest,
+            'title': serie['title'],
             'path': 'data/bank_of_israel/' + hexdigest + '.csv',
             'schema': {
                 'fields': [
@@ -105,39 +120,8 @@ def add_series_to_resources(series):
         }
         datapackage['resources'].append(resource)
 
-# resource = parameters['target-resource']
-# resource[PROP_STREAMING] = True
-# resource['schema'] = {
-#     'fields': [
-#
-#         {
-#             'name': 'description',
-#             'type': 'string'
-#         },
-#         {
-#             'name': 'units',
-#             'type': 'string'
-#         },
-#         {
-#             'name': 'season_adjusted',
-#             'type': 'string'
-#         },
-#         {
-#             'name': 'date',
-#             # 'type': 'string',
-#             'type': 'date',
-#             'format': '%d-%m-%Y'
-#         },
-#         {
-#             'name': 'value',
-#             'type': 'string'
-#         },
-#     ]
-# }
-# datapackage['resources'].append(resource)
 
-
-series = sum([get_series(sid) for sid in sids], [])
-add_series_to_resources(series)
-reports = get_all_reports(series)
+series_sum = get_all_series()
+add_series_to_resources(series_sum)
+reports = get_all_reports(series_sum)
 spew(datapackage, chain(res_iter, reports))
