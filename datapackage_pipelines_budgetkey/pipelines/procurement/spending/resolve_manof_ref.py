34caadb599e62a7ac2a2734e35c69254f80a3aa6
Adam Kariv <adam.kariv@gmail.com>
2018-06-06 15:04:12 +0300

    Locate payments for each tender

--- datapackage_pipelines_budgetkey/pipelines/procurement/spending/resolve_manof_ref.py
+++ datapackage_pipelines_budgetkey/pipelines/procurement/spending/resolve_manof_ref.py
@@ -8,7 +8,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.exc import ProgrammingError, OperationalError
 
 TK = 'tender_key'
-db_table = 'procurement_tenders_processed'
+db_table = 'procurement_tenders'
 connection_string = os.environ['DPP_DB_ENGINE']
 engine = create_engine(connection_string)
 
@@ -22,25 +22,28 @@ for result in engine.execute(f'select {to_select} from {db_table}'):
 logging.info('Collected %d tenders and exemptions', len(all_tenders))
 
 def modify_datapackage(dp, *_):
-    dp['resources'][0]['schema']['fields'].extend([dict(
+    dp['resources'][0]['schema']['fields'].append(dict(
         name = TK,
         type = 'string'
-    ) for k in key_fields])
+    ))
     return dp
 
+failed = set()
 def process_row(row, *_):
     mf = row['manof_ref']
     if mf:
         mf = mf.strip()
-    if mf:
-        for t in all_tenders:
-            if ((t[0] and t[0] in mf) or 
-                (t[2] and len(t[2]) > 3 and t[2] != 'non' and t[2] in mf)):
-                row[TK] = json.dumps(list(t))
-                break
+    if mf and len(mf)>3:
+        if mf not in failed:
+            for t in all_tenders:
+                if ((t[0] and t[0] in mf) or 
+                    (t[2] and len(t[2]) > 3 and t[2] != 'non' and t[2] in mf)):
+                    row[TK] = json.dumps(list(t))
+                    break
         if TK not in row:
             row[TK] = None
             logging.info('Failed to find reference for "%s"', mf)
+            failed.add(mf)
     else:
         row[TK] = None
     return row
---
+++ datapackage_pipelines_budgetkey/pipelines/procurement/tenders/locate_payments.py
@@ -0,0 +1,44 @@
+import os
+import logging
+import json
+
+from datapackage_pipelines.wrapper import process
+
+from sqlalchemy import create_engine
+from sqlalchemy.exc import ProgrammingError, OperationalError
+
+connection_string = os.environ['DPP_DB_ENGINE']
+engine = create_engine(connection_string)
+
+query = """
+with a as (
+select publisher_name, purchasing_unit, purpose, budget_code, budget_title, entity_name, entity_id, entity_kind,
+       executed, volume, currency, explanation, payments, start_date, end_date, 
+       jsonb_array_elements_text(tender_key) as tender_key
+)
+select * from a where tender_key='%'
+"""
+
+def get_all_contracts(key):
+    return [dict(r) for r in engine.execute(query % key)]
+
+
+def modify_datapackage(dp, *_):
+    dp['resources'][0]['schema']['fields'].append({
+        'name': 'contracts',
+        'type': 'array',
+        'es:itemType': 'object',
+        'es:index': False
+
+     })
+    return dp
+
+def process_row(row, *_):
+    key_fields = ('publication_id', 'tender_type', 'tender_id')
+    key = json.dumps(str(row[k]) for k in key_fields)
+    row['contracts'] = get_all_contracts(key)
+    return row
+
+if __name__ == '__main__':
+    process(modify_datapackage=modify_datapackage,
+            process_row=process_row)
