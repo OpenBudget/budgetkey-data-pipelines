b81db8586f2d91efe547d62d5e6e8bf8c94cf410
Adam Kariv <adam.kariv@gmail.com>
2018-07-21 22:38:24 +0300

    dpdumper fix cross-device link

--- datapackage_pipelines_budgetkey/pipelines/budgetkey/elasticsearch/dpdumper.py
+++ datapackage_pipelines_budgetkey/pipelines/budgetkey/elasticsearch/dpdumper.py
@@ -7,10 +7,10 @@ from datapackage_pipelines.wrapper import process
 def modify_datapackage(dp, parameters, _):
     os.makedirs(parameters['out-path'], exist_ok=True)
     if dp:
-        with tempfile.NamedTemporaryFile(delete=False, mode='w') as tmp:
+        filename = os.path.join(parameters['out-path'], 'datapackage.json')
+        with open(filename + '.tmp', 'w') as tmp:
             json.dump(dp, tmp)
-            tmp.close()
-            os.rename(tmp.name, os.path.join(parameters['out-path'], 'datapackage.json'))
+        os.rename(filename+'.tmp', filename)
     return dp
 
 
