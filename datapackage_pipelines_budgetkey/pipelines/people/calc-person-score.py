cea52323c7c68e17fa3446afa75e5cfc2dda74b1
Adam Kariv <adam.kariv@gmail.com>
2018-07-24 11:58:24 +0300

    Fix people indexing

--- datapackage_pipelines_budgetkey/pipelines/people/calc-person-score.py
+++ datapackage_pipelines_budgetkey/pipelines/people/calc-person-score.py
@@ -4,11 +4,15 @@ from datapackage_pipelines.wrapper import process
 old = datetime.datetime.fromtimestamp(0)
 
 def modify_datapackage(dp, *_):
-    dp['resources'][0]['schema']['fields'].append({
+    dp['resources'][0]['schema']['fields'].extend([{
         'name': 'score',
         'type': 'number',
         'es:score-column': True
-    })
+    }, {
+        'name': 'person_key',
+        'type': 'string',
+        'es:title': True
+    }])
     return dp
 
 
@@ -21,6 +25,7 @@ def start_date(x):
 
 def process_row(row, *_):
     row['score'] = 1
+    row['person_key'] = row['key']
     row['details'] = sorted(row['details'], key=start_date)
     return row
 
