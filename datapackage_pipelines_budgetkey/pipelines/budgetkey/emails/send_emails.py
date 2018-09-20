2ca1ba7f77106bc8045ef1c773661c8f76df8c23
Adam Kariv <adam.kariv@gmail.com>
2018-09-20 10:42:14 +0300

    fixes to emails, bump budget

--- datapackage_pipelines_budgetkey/pipelines/budgetkey/emails/send_emails.py
+++ datapackage_pipelines_budgetkey/pipelines/budgetkey/emails/send_emails.py
@@ -56,7 +56,7 @@ def process_row(row, *_):
                 logging.error('Failed to parse properties %s', e)
                 continue
             if props and 'displayDocsTypes' in props:
-                if 'tenders' in props['displayDocsTypes']:
+                if any(x in props['displayDocsTypes'] for x in ('tenders', 'all'):
                   terms.append(dict(
                       term=props['term'],
                       query_url=query_url(props['term'], filters)
