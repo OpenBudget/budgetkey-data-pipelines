6bfc010343b0bca71a6400d5ef81e465ecf7f25d
Adam Kariv <adam.kariv@gmail.com>
2018-10-18 16:55:15 +0300

    resource_without_excel_formulas refactor

--- datapackage_pipelines_budgetkey/processors/resource_without_excel_formulas.py
+++ datapackage_pipelines_budgetkey/processors/resource_without_excel_formulas.py
@@ -10,36 +10,36 @@ from datapackage_pipelines.wrapper import ingest
 from datapackage_pipelines_budgetkey.common.cookie_monster import cookie_monster_get
 
 
-with ingest() as ctx:
-    parameters, datapackage, res_iter = tuple(ctx)
+with tempfile.NamedTemporaryFile(suffix='.csv') as out:
+    with ingest() as ctx:
+        parameters, datapackage, res_iter = tuple(ctx)
 
-    url = parameters.get('url')
-    resource = parameters.get('resource')
-    resource[PROP_STREAMING] = True
+        url = parameters.get('url')
+        resource = parameters.get('resource')
+        resource[PROP_STREAMING] = True
 
-    content = requests.get(url).content
-    if len(content) < 1024:
-        content = cookie_monster_get(url)
+        content = requests.get(url).content
+        if len(content) < 1024:
+            content = cookie_monster_get(url)
 
-    content = content.replace(b'\n="', b'\n"')
-    content = content.replace(b',="', b',"')
+        content = content.replace(b'\n="', b'\n"')
+        content = content.replace(b',="', b',"')
 
-    out = tempfile.NamedTemporaryFile(suffix='.csv', delete=False)
-    out.write(content)
-    out.flush()
+        out.write(content)
+        out.flush()
 
-    logging.info('downloaded from %s %d bytes: %r', url, len(content), content[:1000])
+        logging.info('downloaded from %s %d bytes: %r', url, len(content), content[:1000])
 
-    datapackage['resources'].append(resource)
+        datapackage['resources'].append(resource)
 
-    stream = \
-        tabulator.Stream('file://'+out.name, **parameters.get('tabulator', {}))\
-        .open()
-    resource['schema'] = {
-        'fields': [
-            {'name': h, 'type': 'string'}
-            for h in stream.headers
-        ]
-    }
+        stream = \
+            tabulator.Stream('file://'+out.name, **parameters.get('tabulator', {}))\
+            .open()
+        resource['schema'] = {
+            'fields': [
+                {'name': h, 'type': 'string'}
+                for h in stream.headers
+            ]
+        }
 
-    ctx.resource_iterator = itertools.chain(res_iter, [stream.iter(keyed=True)])
+        ctx.resource_iterator = itertools.chain(res_iter, [stream.iter(keyed=True)])
