f975444aa7daf21203a96e9a460bd0d8b2234b93
Adam Kariv <adam.kariv@gmail.com>
2018-11-26 22:07:36 +0200

    cont

--- datapackage_pipelines_budgetkey/processors/allstar_dumper.py
+++ datapackage_pipelines_budgetkey/processors/allstar_dumper.py
@@ -15,48 +15,52 @@ def resource_url(out_path, res_path):
     return 'https://next.obudget.org/{}'.format(res_path)
 
 
+def dump_to_datahub(ctx):
+    if any(os.environ.get('DATAHUB_'+x) is None
+            for x in ('OWNER', 'OWNERID', 'ENDPOINT', 'TOKEN')):
+        return
+
+    dp_path = ctx.stats[STATS_DPP_KEY][STATS_OUT_DP_URL_KEY]
+    datapackage = json.load(open(dp_path))
+    flowspec = {
+        'meta': {
+            'owner': os.environ['DATAHUB_OWNER'],
+            'ownerid': os.environ['DATAHUB_OWNERID'],
+            'dataset': datapackage['name'],
+            'version': 1,
+            'findability': 'published'
+        },
+        'inputs': [
+            {
+                'kind': 'datapackage',
+                'url': resource_url(dp_path, 'datapackage.json'),
+                'parameters': {
+                    'descriptor': datapackage,
+                    'resource-mapping': dict(
+                        (r['name'], resource_url(dp_path, r['path']))
+                        for r in datapackage['resources']
+                    )
+                }
+            }
+        ]
+    }
+    resp = requests.get(os.environ['DATAHUB_ENDPOINT']+'/auth/authorize', 
+                        params={'service': 'source'},
+                            headers={
+                                'Auth-Token': os.environ['DATAHUB_TOKEN']
+                            }).json()
+    token = resp['token']
+    resp = requests.post(os.environ['DATAHUB_ENDPOINT']+'/source/upload', json=flowspec,
+                            headers={
+                                'Auth-Token': token
+                            }
+                        ).json()
+    logging.info('UPLOAD response: %r', resp)
+    assert resp['success'], json.dumps(resp, indent=2)
+
+
 if __name__ == '__main__':
     with ingest() as ctx:
         spew_flow(dtp_flow(ctx.parameters, ctx.stats), ctx)
 
-        if any(os.environ.get('DATAHUB_'+x) is None
-               for x in ('OWNER', 'OWNERID', 'ENDPOINT', 'TOKEN')):
-            return
-
-        dp_path = ctx.stats[STATS_DPP_KEY][STATS_OUT_DP_URL_KEY]
-        datapackage = json.load(open(dp_path))
-        flowspec = {
-            'meta': {
-                'owner': os.environ['DATAHUB_OWNER'],
-                'ownerid': os.environ['DATAHUB_OWNERID'],
-                'dataset': datapackage['name'],
-                'version': 1,
-                'findability': 'published'
-            },
-            'inputs': [
-                {
-                    'kind': 'datapackage',
-                    'url': resource_url(dp_path, 'datapackage.json'),
-                    'parameters': {
-                        'descriptor': datapackage,
-                        'resource-mapping': dict(
-                            (r['name'], resource_url(dp_path, r['path']))
-                            for r in datapackage['resources']
-                        )
-                    }
-                }
-            ]
-        }
-        resp = requests.get(os.environ['DATAHUB_ENDPOINT']+'/auth/authorize', 
-                            params={'service': 'source'},
-                             headers={
-                                 'Auth-Token': os.environ['DATAHUB_TOKEN']
-                             }).json()
-        token = resp['token']
-        resp = requests.post(os.environ['DATAHUB_ENDPOINT']+'/source/upload', json=flowspec,
-                             headers={
-                                 'Auth-Token': token
-                             }
-                            ).json()
-        logging.info('UPLOAD response: %r', resp)
-        assert resp['success'], json.dumps(resp, indent=2)
+        dump_to_datahub(ctx)
