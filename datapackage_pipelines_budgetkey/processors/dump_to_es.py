787e3c48a62e4fed7024dd2607a2d965f9765eea
Adam Kariv <adam.kariv@gmail.com>
2018-11-28 09:10:28 +0200

    cont

--- datapackage_pipelines_budgetkey/processors/dump_to_es.py
+++ datapackage_pipelines_budgetkey/processors/dump_to_es.py
@@ -76,8 +76,11 @@ class DumpToElasticSearch(ESDumper):
                 yield from super(DumpToElasticSearch, self)\
                                     .handle_resource(self.format_datetime_rows(spec, resource), 
                                         spec, parameters, datapackage)
-            except Exception:
-                logging.exception('DUMP TO ES ERROR')
+            except Exception as e:
+                logging.error('DUMP TO ES ERROR %s', str(e))
+                if hasattr(e, errors):
+                    logging.error('errors %r', e.errors)
+                logging.exception('TB')
                 raise
         return func()
 
