654d09eec9804bf3766e6becb1e9c62c8c6a811e
Adam Kariv <adam.kariv@gmail.com>
2019-04-01 11:07:27 +0300

    Add roof titles to budget transfer summaries

--- datapackage_pipelines_budgetkey/pipelines/budget/national/changes/processed/summarize.py
+++ datapackage_pipelines_budgetkey/pipelines/budget/national/changes/processed/summarize.py
@@ -1,4 +1,12 @@
 from datapackage_pipelines.wrapper import process
+import datapackage
+
+ROOF_TITLES = datapackage.Package('https://next.obudget.org/datapackages/budget/national/processed/roof-names/datapackage.json')
+ROOF_TITLES = ROOF_TITLES.resources[0].iter(keyed=True)
+ROOF_TITLES = dict(
+    ((x['year'], x['code']), x['title'])
+    for x in ROOF_TITLES
+)
 
 
 def process_row(row, row_index,
@@ -20,6 +28,8 @@ def process_row(row, row_index,
             roofs.setdefault(code, 0)
             roofs[code] += amount
         roofs = sorted(roofs.items(), key=lambda r: -abs(r[1]))
+        for r in roofs:
+            r.append(ROOF_TITLES.get((row['year'], r[0]), r[0]))
         row['summary'] = {
             'title': row['req_title'][0],
             'kind': row['change_title'][0],
