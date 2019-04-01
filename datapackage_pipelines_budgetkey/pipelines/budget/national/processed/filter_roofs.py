34443b2821601d2bc9d1a070cecbcc02b3731f1f
Adam Kariv <adam.kariv@gmail.com>
2019-04-01 09:39:02 +0300

    Add roof names dataset

---
+++ datapackage_pipelines_budgetkey/pipelines/budget/national/processed/filter_roofs.py
@@ -0,0 +1,20 @@
+from datapackage_pipelines.wrapper import spew, ingest
+
+parameters, datapackage, res_iter = ingest()
+
+
+def process_first(rows):
+    for row in rows:
+        if len(row.get('code') == 4):
+            yield row
+
+
+def process_resources(res_iter_):
+    first = next(res_iter_)
+    yield process_first(first)
+
+    for res in res_iter_:
+        yield res
+
+
+spew(datapackage, process_resources(res_iter))
