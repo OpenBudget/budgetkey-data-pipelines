7e7fbdbc92916220c4f6a58edf46852ca1fe48b6
Adam Kariv <adam.kariv@gmail.com>
2019-04-01 21:14:32 +0300

    Calc report scores

---
+++ datapackage_pipelines_budgetkey/pipelines/reports/calc-score.py
@@ -0,0 +1,27 @@
+import math
+import datetime
+import logging
+from datapackage_pipelines.wrapper import process
+
+curyear = datetime.datetime.now().date().year + 1
+
+def modify_datapackage(dp, *_):
+    dp['resources'][0]['schema']['fields'].append({
+        'name': 'score',
+        'type': 'number',
+        'es:score-column': True
+    })
+    return dp
+
+
+def process_row(row, *_):    
+    row['score'] = 10
+    report = row.get('details',{}).get('report',{}).get('total',{})
+    count1 = report.get('total_amount')
+    count2 = report.get('count')
+    row['score'] += count1 or count2s
+    return row
+
+
+process(modify_datapackage=modify_datapackage,
+        process_row=process_row)
