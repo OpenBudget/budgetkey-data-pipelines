5a3993045d6d4e2f38ec61f11af34139ec4c1874
Adam Kariv <adam.kariv@gmail.com>
2019-04-01 20:04:57 +0300

    Add scores to criteria and calls for bids

---
+++ datapackage_pipelines_budgetkey/pipelines/procurement/calls_for_bids/calc-score.py
@@ -0,0 +1,23 @@
+import math
+import datetime
+import logging
+from datapackage_pipelines.wrapper import process
+
+curyear = datetime.datetime.now().date().year + 1
+
+def modify_datapackage(dp, *_):
+    dp['resources'][0]['schema']['fields'].append({
+        'name': 'score',
+        'type': 'number',
+        'es:score-column': True
+    })
+    return dp
+
+
+def process_row(row, *_):    
+    row['score'] = 10
+    return row
+
+
+process(modify_datapackage=modify_datapackage,
+        process_row=process_row)
