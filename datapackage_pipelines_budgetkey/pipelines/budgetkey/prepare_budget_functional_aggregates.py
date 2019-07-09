10dc52ef52e28c3edd1a0b1c20120d3ea0b14090
Adam Kariv <adam.kariv@gmail.com>
2019-07-09 09:59:16 +0300

    +

--- datapackage_pipelines_budgetkey/pipelines/budgetkey/prepare_budget_functional_aggregates.py
+++ datapackage_pipelines_budgetkey/pipelines/budgetkey/prepare_budget_functional_aggregates.py
@@ -23,6 +23,7 @@ def modify_datapackage(dp, *_):
         {'name': 'net_revised', 'type': 'integer'},
         {'name': 'year', 'type': 'integer'},
     ]
+    df['resources'][0]['name'] = 'budget-functional-aggregates'
     return dp
 
 process(modify_datapackage=modify_datapackage,
