a1e8c1ad44b4685c505e099ff17ce535c8c33133
Adam Kariv <adam.kariv@gmail.com>
2019-10-01 20:43:00 +0300

    Cosmetic fix to startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo 'Starting BudgetKey DPP!'
 
-(ssh adam@tzabar.obudget.org "docker system prune -fa  && docker pull budgetkey/budgetkey-data-pipelines") || true
+(ssh adam@tzabar.obudget.org "docker system prune -f -a  && docker pull budgetkey/budgetkey-data-pipelines") || true
 
 /dpp/docker/run.sh server
