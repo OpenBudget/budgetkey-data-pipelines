2d2221de7db5dec63103b2afe39741f4c14817f9
Adam Kariv <adam.kariv@gmail.com>
2019-10-03 11:33:57 +0300

    Force use of private key location

--- datapackage_pipelines_budgetkey/common/google_chrome.py
+++ datapackage_pipelines_budgetkey/common/google_chrome.py
@@ -23,7 +23,10 @@ class google_chrome_driver():
         self.client._policy = paramiko.WarningPolicy()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        self.client.connect(username=username, hostname=self.hostname)
+        try:
+            self.client.connect(username=username, hostname=self.hostname, key_filename='/home/dpp/.ssh/id_rsa')
+        except:
+            self.client.connect(username=username, hostname=self.hostname)
         stdin, stdout, stderr = self.client.exec_command(cmd)
 
         self.docker_container = None
