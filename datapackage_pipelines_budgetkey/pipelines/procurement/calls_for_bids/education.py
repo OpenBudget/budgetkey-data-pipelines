e3812f9e40a1fbfda2bea60155eeb824321a351c
Adam Kariv <adam.kariv@gmail.com>
2019-10-23 17:53:33 +0300

    Fix education documents, target_audience

--- datapackage_pipelines_budgetkey/pipelines/procurement/calls_for_bids/education.py
+++ datapackage_pipelines_budgetkey/pipelines/procurement/calls_for_bids/education.py
@@ -39,7 +39,7 @@ def flow(*_):
             page_title=['Title'],
             start_date=['PobKKPublishingDate'],
             claim_date=['PobLastDate'],
-            target_audience=['PobBudgetEntitties'],
+            target_audience_x=['PobBudgetEntitties'],
             description=['PobTaktzir'],
             email=['PobPedagogyContactHtml'],
             publishing_unit_x=['PobYechida'],
@@ -55,13 +55,19 @@ def flow(*_):
         DF.add_field('tender_id', 'string', '0', resources=-1),
         DF.add_field('tender_type_he', 'string', 'קול קורא', resources=-1),
         DF.add_field('contact', 'string', lambda row: extract_hebrew(row, 'email'), resources=-1),
+        DF.add_field('target_audience', 'string', lambda row: extract_hebrew(row, 'target_audience_x'), resources=-1),
         DF.add_field('contact_email', 'string', lambda row: extract_email(row, 'email'), resources=-1),
         DF.add_field('publishing_unit', 'string', lambda row: row['publishing_unit_x'][0]['PobYechida'], resources=-1),
         DF.add_field('budget_code', 'string', lambda row: extract_budget_code(row, 'budget_code_x'), resources=-1),
-        DF.add_field('documents', 'array', lambda row: [dict(title=row['att_title'], link=row['att_url'])], resources=-1),
-        DF.delete_fields(['email', 'publishing_unit_x', 'budget_code_x', 'att_title', 'att_url'], resources=-1),
         DF.set_type('start_date', type='date', format='%d/%m/%Y %H:%M:%S'),
         DF.set_type('claim_date', type='datetime', format='%d/%m/%Y %H:%M:%S'),
+        DF.add_field('documents', 'array',
+                     lambda row: [dict(
+                         description=row['att_title'],
+                         link=row['att_url'],
+                         update_time=str(row['start_date'])
+                     )], resources=-1),
+        DF.delete_fields(['email', 'publishing_unit_x', 'budget_code_x', 'att_title', 'att_url', 'target_audience_x'], resources=-1),
         calculate_publication_id(6),
         DF.update_resource(-1, **{'dpp:streaming': True})
     )
