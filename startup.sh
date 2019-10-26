5f6351d1c5e7351890d10c2a0ddab052bc6adee3
Adam Kariv <adam.kariv@gmail.com>
2019-10-26 13:18:54 +0300

    Stop all google chromes on restart

--- startup.sh
+++ startup.sh
@@ -2,5 +2,6 @@
 echo 'Starting BudgetKey DPP!'
 
 (ssh adam@tzabar.obudget.org "docker system prune -f -a  && docker pull budgetkey/budgetkey-data-pipelines") || true
+(ssh adam@tzabar.obudget.org "docker ps | grep google-chrome | cut -c1-12 | xargs docker stop") || true
 
 /dpp/docker/run.sh server
