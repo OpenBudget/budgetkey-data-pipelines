d1f5044ce216a0f141ff9f990ffc459e27c2376d
Adam Kariv <adam.kariv@gmail.com>
2019-11-19 13:47:48 +0200

    Viewport to sitemap

--- datapackage_pipelines_budgetkey/processors/build_sitemaps_index.py
+++ datapackage_pipelines_budgetkey/processors/build_sitemaps_index.py
@@ -36,7 +36,7 @@ def generate_sitemap_index(rows):
 
     with open('/var/datapackages/sitemaps/sitemap.html', 'w') as out:
         out.write('''<!DOCTYPE html>
-    <html><head><meta charset="utf-8"></head>
+    <html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"></head>
     <body><ul>
     ''')
         for row in rows:
