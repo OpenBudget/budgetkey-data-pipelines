c2e7e4270752b5add701ac24bf726093e00d18bd
Adam Kariv <adam.kariv@gmail.com>
2019-12-24 14:10:37 +0200

    Ignore bad rows in supports scraper

--- datapackage_pipelines_budgetkey/pipelines/supports/fix_values.py
+++ datapackage_pipelines_budgetkey/pipelines/supports/fix_values.py
@@ -1,7 +1,10 @@
+import logging
 from datapackage_pipelines.wrapper import process
 
 
 def process_row(row, *_):
+    if len(row['budget_code']) != 8:
+        row['budget_code'] = '9' * 8
     if row['year_paid'] == 'אינו מוקצה':
         row['year_paid'] = ''
     row['budget_code'] = '00' + row['budget_code']
