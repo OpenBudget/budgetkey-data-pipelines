be6f9bddffd93039622e5e68049f1f4c1b8c284b
Adam Kariv <adam.kariv@gmail.com>
2020-01-20 15:59:28 +0200

    More powerful docker kill command

--- startup.sh
+++ startup.sh
@@ -2,6 +2,6 @@
 echo 'Starting BudgetKey DPP!'
 
 (ssh adam@tzabar.obudget.org "docker system prune -f -a  && docker pull budgetkey/budgetkey-data-pipelines") || true
-(ssh adam@tzabar.obudget.org "docker ps | grep google-chrome | cut -c1-12 | xargs docker stop") || true
+(ssh adam@tzabar.obudget.org "docker ps | grep -v wernight/phantomjs | grep -v CONTAINER | cut -c1-12 | xargs docker kill -s 9") || true
 
 /dpp/docker/run.sh server
