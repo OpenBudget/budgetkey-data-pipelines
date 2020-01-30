517a821891fdd737648b8af1dda2d3ac94a93685
Adam Kariv <adam.kariv@gmail.com>
2020-01-30 14:33:50 +0200

    +

--- datapackage_pipelines_budgetkey/pipelines/budget/national/original/only_years.py
+++ datapackage_pipelines_budgetkey/pipelines/budget/national/original/only_years.py
@@ -2,7 +2,7 @@ import dataflows as DF
 
 
 def filter_years(rows):
-    return (row for row in rows if 'שנה' in row)
+    return (row for row in rows if row.get('שנה') and row.get('סוג תקציב'))
 
 
 def flow(*_):
