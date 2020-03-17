b7e77892d2a7fbb372f398e8bc9b63fc28872d28
Adam Kariv <adam.kariv@gmail.com>
2020-03-17 18:00:24 +0200

    Bump ottoman

--- datapackage_pipelines_budgetkey/pipelines/entities/ottoman/fix_values.py
+++ datapackage_pipelines_budgetkey/pipelines/entities/ottoman/fix_values.py
@@ -1,4 +1,5 @@
 from datapackage_pipelines.wrapper import process
 from datapackage_pipelines_budgetkey.pipelines.entities.ottoman.ottoman_utils import process_row
 
+
 process(process_row=process_row)
