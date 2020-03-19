6527aadd6fd5df09936e945e8af7373e676d7daa
Adam Kariv <adam.kariv@gmail.com>
2020-03-19 16:33:34 +0200

    Remove trailing '.' in ottoman

--- datapackage_pipelines_budgetkey/pipelines/entities/ottoman/fix_values.py
+++ datapackage_pipelines_budgetkey/pipelines/entities/ottoman/fix_values.py
@@ -1,5 +1,4 @@
 from datapackage_pipelines.wrapper import process
 from datapackage_pipelines_budgetkey.pipelines.entities.ottoman.ottoman_utils import process_row
 
-
 process(process_row=process_row)
--- datapackage_pipelines_budgetkey/pipelines/entities/ottoman/ottoman_utils.py
+++ datapackage_pipelines_budgetkey/pipelines/entities/ottoman/ottoman_utils.py
@@ -2,6 +2,6 @@ def process_row(row, *_):
     if len(row['id']) < 9:
         return None
     row['id'] = row['id'][:9]
-    row['name'] = row['name'].strip()
+    row['name'] = row['name'].strip(' \n\t.')
     row['address'] = row['address'].strip() if row['address'] else None
     return row
