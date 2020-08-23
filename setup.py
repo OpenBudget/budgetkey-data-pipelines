021bef19cd263afeee9673d0474e4fbaf3743850
Adam Kariv <adam.kariv@gmail.com>
2020-08-23 12:05:20 +0300

    Bump dataflows

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
                       'geocoder',
                       'boto3',
                       'paramiko',
-                      'dataflows>=0.0.65',
+                      'dataflows>=0.1.5',
 		     ],
     extras_require={'develop': ["tox", "datapackage-pipelines"]},
     entry_points={'console_scripts': ['budgetkey-dpp = datapackage_pipelines_budgetkey.cli:main']}
