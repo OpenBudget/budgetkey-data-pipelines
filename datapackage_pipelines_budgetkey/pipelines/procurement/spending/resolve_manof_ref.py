99fe3e861850ae3cd2d0548cf4c7484d415cd5f4
Adam Kariv <adam.kariv@gmail.com>
2020-09-09 21:49:47 +0300

    debug manof ref

--- datapackage_pipelines_budgetkey/pipelines/procurement/spending/resolve_manof_ref.py
+++ datapackage_pipelines_budgetkey/pipelines/procurement/spending/resolve_manof_ref.py
@@ -55,10 +55,13 @@ def process_row(row, *_):
     row[TK] = None
     row[TK + '_simple'] = None
     for mf in parts:
+        debug = '40/2011' in mf
         if mf not in failed:
             if mf not in DISALLOWED:
                 if mf in all_tenders_dict:
+                    if debug: logging.info('DEBUG: %r %r', mf, row)
                     options = all_tenders_dict[mf]
+                    if debug: logging.info('DEBUG: options: %r', options)
                     if len(options) == 1:
                         selected = options[0]
                     else:
@@ -66,9 +69,11 @@ def process_row(row, *_):
                         selected = options[0]
                         if publisher_name:
                             options = dict((k[3], k) for k in options if k[3])
+                            if debug: logging.info('DEBUG: publisher: %r %r', publisher_name, options)
                             publishers = list(options.keys())
                             if len(publishers) > 0:
                                 selected, score = fw_process.extractOne(publisher_name, publishers, processor=fuzz.ratio)
+                                if debug: logging.info('DEBUG: selected: %r %r', selected, score)
                                 if score < 60:
                                     logging.info('Failed to find publisher match for %r: %r', publisher_name, list(options.values()))
                                 selected = options[selected]
