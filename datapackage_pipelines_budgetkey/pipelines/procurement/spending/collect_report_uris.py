e3eeebb955769e441824f04f1ed5962e0c2267c1
Adam Kariv <adam.kariv@gmail.com>
2021-06-24 11:09:01 +0300

    skip pdfs in spending

--- datapackage_pipelines_budgetkey/pipelines/procurement/spending/collect_report_uris.py
+++ datapackage_pipelines_budgetkey/pipelines/procurement/spending/collect_report_uris.py
@@ -47,6 +47,8 @@ def get_all():
                 continue
             for f in r.pop('File'):
                 f.update(r)
+                if f['FileName'].endswith('pdf'):
+                    continue
                 yield {
                     'report-url': f'https://www.gov.il/BlobFolder/dynamiccollectorresultitem/{base}/he/{f["FileName"]}',
                     'report-title': f['Title'],
