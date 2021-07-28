418b3b301f74985e5f8501888afa3b67d1e9a202
Adam Kariv <adam.kariv@gmail.com>
2021-07-28 11:22:04 +0300

    Add endowments

---
+++ datapackage_pipelines_budgetkey/pipelines/entities/endowments/endowments.py
@@ -0,0 +1,19 @@
+import dataflows as DF
+
+def flow(*_):
+    return DF.Flow(
+        DF.load('endowments.xlsx'),
+        DF.set_type('id', type='string'),
+        DF.set_type('registration_year', type='integer'),
+        DF.set_type('endower', type='string'),
+        DF.set_type('areas_of_activity', type='array', **{'es:type': 'string'}, transform=lambda x: x.split(',') if x else []),
+        DF.set_type('areas_of_activity_secondary', type='array', **{'es:type': 'string'}, transform=lambda x: x.split(',') if x else []),
+        DF.set_type('endowment_size', type='number'),
+        DF.set_type('last_report_year', type='integer'),
+        DF.set_type('management_method', type='string'),
+        DF.update_resource(-1, **{'name': 'endowments'}),
+        DF.update_resource(-1, **{'dpp:streaming': True}),
+    )
+
+if __name__ == '__main__':
+    DF.Flow(flow(), DF.printer()).process()
