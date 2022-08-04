fa9501a4e0c384d45eee00e278499c7a4529e25e
Adam Kariv <adam.kariv@gmail.com>
2022-08-04 18:50:41 +0300

    Add redirect for top level muni budgets to latest year

--- datapackage_pipelines_budgetkey/pipelines/budgetkey/elasticsearch/add_redirects_for_muni_budgets.py
+++ datapackage_pipelines_budgetkey/pipelines/budgetkey/elasticsearch/add_redirects_for_muni_budgets.py
@@ -19,6 +19,16 @@ def expand_redirects():
                         doc_id=new_doc_id,
                         __redirect=doc_id
                     )
+            if len(code) < 4:
+                new_doc_id = '/'.join([kind, muni_code, code])
+                yield dict(
+                    code=code,
+                    muni_code=muni_code,
+                    title=h['title'],
+                    year=h['year'],
+                    doc_id=new_doc_id,
+                    __redirect=doc_id
+                )
     return func
 
 
