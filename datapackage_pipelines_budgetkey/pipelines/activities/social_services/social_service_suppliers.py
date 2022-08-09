cd887e4530b7bf92f9ac1d181871840fd75f664a
Adam Kariv <adam.kariv@gmail.com>
2022-08-09 11:50:55 +0300

    Add service ids to soproc suppliers file

--- datapackage_pipelines_budgetkey/pipelines/activities/social_services/social_service_suppliers.py
+++ datapackage_pipelines_budgetkey/pipelines/activities/social_services/social_service_suppliers.py
@@ -8,16 +8,31 @@ def unwind():
                     if supplier['entity_id']:
                         yield dict(
                             entity_id=supplier['entity_id'],
-                            soproc_supplier=True,
+                            services=row['id'],
                         )
     return func
 
-def flow(*_):
+def flow(parameters, *_):
+    prod = not parameters.get('debug', False)
+    source = '/var/datapackages/activities/social_services/datapackage.json' if prod else 'https://next.obudget.org/datapackages/activities/social_services/datapackage.json'
     return DF.Flow(
-        DF.load('/var/datapackages/activities/social_services/datapackage.json'),
+        DF.load(source),
         DF.add_field('entity_id', 'string'),
-        DF.add_field('soproc_supplier', 'boolean'),
+        DF.add_field('services', 'string'),
         unwind(),
-        DF.select_fields(['entity_id', 'soproc_supplier']),
+        DF.update_resource(-1, name='soproc_suppliers'),
+        DF.join_with_self('soproc_suppliers', ['entity_id'], dict(
+            entity_id=None,
+            services=dict(aggregate='set'),
+        )),
+        DF.add_field('soproc_supplier', 'boolean', True),
+        DF.select_fields(['entity_id', 'soproc_supplier', 'services']),
         DF.dump_to_path('/var/datapackages/activities/social_services_suppliers'),
     )
+
+if __name__=='__main__':
+    parameters = dict(debug=True)
+    DF.Flow(
+        flow(parameters),
+        DF.printer()
+    ).process()
