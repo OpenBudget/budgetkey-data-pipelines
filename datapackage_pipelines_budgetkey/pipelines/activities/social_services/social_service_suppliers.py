dd869f782000283d3bb8b3a77042c84f3f24dc33
Adam Kariv <adam.kariv@gmail.com>
2022-09-05 12:11:52 +0300

    Add entity_kind to soproc_suppliers

--- datapackage_pipelines_budgetkey/pipelines/activities/social_services/social_service_suppliers.py
+++ datapackage_pipelines_budgetkey/pipelines/activities/social_services/social_service_suppliers.py
@@ -9,6 +9,7 @@ def unwind():
                     if supplier['entity_id']:
                         yield dict(
                             entity_id=supplier['entity_id'],
+                            entity_kind=supplier['entity_kind'],
                             services=row['id'],
                             updated_at=row['updated_at'],
                         )
@@ -21,16 +22,18 @@ def flow(parameters, *_):
     return DF.Flow(
         DF.load(source),
         DF.add_field('entity_id', 'string'),
+        DF.add_field('entity_kind', 'string'),
         DF.add_field('services', 'string'),
         unwind(),
         DF.update_resource(-1, name='soproc_suppliers'),
         DF.join_with_self('soproc_suppliers', ['entity_id'], dict(
             entity_id=None,
+            entity_kind=None,
             services=dict(aggregate='set'),
             updated_at=dict(aggregate='max')
         )),
         DF.add_field('soproc_supplier', 'boolean', True),
-        DF.select_fields(['entity_id', 'soproc_supplier', 'services', 'updated_at']),
+        DF.select_fields(['entity_id', 'soproc_supplier', 'services', 'updated_at', 'entity_kind']),
         
         DF.duplicate('soproc_suppliers', 'new_soproc_suppliers'),
         DF.update_resource(-1, path='new_soproc_suppliers.csv'),
