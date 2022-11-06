c9ae86cc4c129457ce8c86fcd5beb8c7fee9efe1
Adam Kariv <adam.kariv@gmail.com>
2022-11-06 09:53:42 +0200

    Add soproc_id and soproc_name to tenders processed

--- datapackage_pipelines_budgetkey/pipelines/activities/social_services/social_service_tenders.py
+++ datapackage_pipelines_budgetkey/pipelines/activities/social_services/social_service_tenders.py
@@ -14,19 +14,34 @@ def unwind():
                             publication_id=publication_id,
                             tender_key = key,
                             soproc_tender=True,
+                            soproc_id=row['id'],
+                            soproc_name=row['name'],
                         )
     return func
 
-def flow(*_):
+def main(debug=False):
+    BASE = '/var/datapackages/' if not debug else 'https://next.obudget.org/datapackages/'
     return DF.Flow(
-        DF.load('/var/datapackages/activities/social_services/datapackage.json'),
+        DF.load(BASE + 'activities/social_services/datapackage.json'),
         DF.add_field('tender_id', 'string'),
         DF.add_field('publication_id', 'string'),
         DF.add_field('tender_type', 'string'),
         DF.add_field('tender_key', 'string'),
         DF.add_field('soproc_tender', 'boolean'),
+        DF.add_field('soproc_id', 'string'),
+        DF.add_field('soproc_name', 'string'),
         unwind(),
-        DF.select_fields(['tender_id', 'publication_id', 'tender_type', 'tender_key', 'soproc_tender']),
+        DF.select_fields(['tender_id', 'publication_id', 'tender_type', 'tender_key', 'soproc_tender', 'soproc_id', 'soproc_name']),
         DF.dump_to_path('/var/datapackages/activities/social_services_tenders'),
         DF.update_resource(-1, **{'dpp:streaming': True}),
     )
+
+def flow(*_):
+    return main()
+
+
+if __name__ == '__main__':
+    DF.Flow(
+        main(debug=True),
+        DF.printer()
+    ).process()
