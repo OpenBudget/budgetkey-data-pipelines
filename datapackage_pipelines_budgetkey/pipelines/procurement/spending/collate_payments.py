e8bd5bb5d5905cb6be14f500a961646fea5f8384
Adam Kariv <adam.kariv@gmail.com>
2023-09-16 09:48:01 +0300

    publisher_key

--- datapackage_pipelines_budgetkey/pipelines/procurement/spending/collate_payments.py
+++ datapackage_pipelines_budgetkey/pipelines/procurement/spending/collate_payments.py
@@ -20,7 +20,7 @@ def process_row(row, row_index,
             'executed': float(row['executed']) if row['executed'] is not None else None,
             'volume': float(row['volume']) if row['volume'] is not None else None,
         }
-        row['publisher-key'] = row['purchasing_unit'] or row['publisher'] or row['report-publisher']
+        row['publisher_key'] = row['purchasing_unit'] or row['publisher'] or row['report-publisher']
     return row
 
 
@@ -32,7 +32,7 @@ def modify_datapackage(dp, parameters, stats):
                 'type': 'object'
             })
             resource['schema']['fields'].append({
-                'name': 'publisher-key',
+                'name': 'publisher_key',
                 'type': 'string'
             })
     return dp
