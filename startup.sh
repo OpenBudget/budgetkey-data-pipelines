669993ecb3a111385fb838ea1559e65c6c35d275
Adam Kariv <adam.kariv@gmail.com>
2023-10-09 08:30:59 +0300

    Reboot tzabar each time

--- startup.sh
+++ startup.sh
@@ -3,5 +3,7 @@ echo 'Starting BudgetKey DPP!'
 
 (ssh adam@tzabar.obudget.org "docker system prune -f -a  && docker pull budgetkey/budgetkey-data-pipelines") || true
 (ssh adam@tzabar.obudget.org "docker ps | grep -v wernight/phantomjs | grep -v CONTAINER | cut -c1-12 | xargs docker kill -s 9") || true
+(ssh adam@tzabar.obudget.org "sudo reboot now") || true
+sleep 30
 
 /dpp/docker/run.sh server
