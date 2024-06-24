afef0499dc308c51a09c9d25e3d85e709dc0d69f
Adam Kariv <adam.kariv@gmail.com>
2024-06-24 13:23:52 +0300

    Initial simpledb

---
+++ datapackage_pipelines_budgetkey/pipelines/simpledb/process.py
@@ -0,0 +1,147 @@
+import dataflows as DF
+
+def filtered_budget_code(code):
+    if not code:
+        return None
+    if code.startswith('0000'):
+        return None
+    if code == '00':
+        return None
+    if code.startswith('C'):
+        return None
+    code = code[2:]
+    ret = ''
+    while code:
+        ret += code[:2] + '.'
+        code = code[2:]
+    return ret[:-1]
+
+
+PARAMETERS = dict(
+    budget=dict(
+        source='/var/datapackages/budget/national/processed/with-extras',
+        details='''
+            סעיפי התקציב מספר תקציב המדינה.
+            המידע הוא לכלל שנות התקציב מאז 1997 ועד השנה הנוכחית (2024).
+        ''',
+        fields=[
+            dict(
+                name='code',
+                description='''
+                    קוד הסעיף התקציבי.
+                    קבוצות של שתי ספרות מופרדות על ידי נקודה.
+                    ככל שהקוד יותר ארוך, כך הסעיף יותר מפורט, למשל:
+                    - 20: תקציב משרד החינוך
+                        סעיפים מסוג זה מכילים תקציבים של כלל המשרדים
+                        נקראים גם סעיפי 2 ספרות
+                    - 20.67: פעולות משלימות
+                        סעיפים ברמה זו מכילים תקציבים של תחומי פעילות
+                        נקראים גם סעיפי 4 ספרות
+                    - 20.67.01: פעילויות ופרוייקטים
+                        סעיפים ברמה זו נקראים גם ״תכניות תקציביות״
+                        נקראים גם סעיפי 6 ספרות
+                    - 20.67.01.42: תמיכה בתנועות נוער
+                        סעיפים ברמה זו נקראים גם ״תקנות תקציביות״
+                        נקראים גם סעיפי 8 ספרות
+                ''',
+                sample_values=['20', '20.67', '20.67.01', '20.67.01.42'],
+                transform=filtered_budget_code,
+                filter=lambda x: x is not None,
+            ),
+            dict(
+                name='title',
+                description='''
+                    כותרת הסעיף התקציבי.
+                    כותרת זו מתארת את תכנית התקציב של הסעיף.
+                    הכותרת בדרך כלל קצרה ולא מאוד תיאורית.
+                ''',
+                sample_values=['תמיכה בתנועות נוער', 'מלוות סחירים', 'קורונה - אבטלה']
+            ),
+            dict(
+                name='year',
+                description='''
+                    שנת התקציב.
+                    שנת התקציב ממנו נלקח הסעיף.
+                ''',
+                sample_values=[2017, 2023, 2024]
+            ),
+            dict(
+                name='amount_allocated',
+                description='''
+                    סכום התקציב המתוכנן (נקרא גם תקציב מקורי).
+                    סכום התקציב של הסעיף בשקלים.
+                ''',
+                sample_values=[1000000, 5000000, 10000000],
+                type='number',
+                default=lambda row: row.get('net_allocated')
+            ),
+            dict(
+                name='amount_revised',
+                description='''
+                    סכום התקציב המתוקן, לאחר שבוצעו עליו שינויים במהלך השנה.
+                    נקרא גם תקציב מאושר״ או ״תקציב על שינוייו״.
+                    סכום התקציב של הסעיף לאחר שינויים בשקלים.
+                ''',
+                sample_values=[1000000, 5000000, 10000000],
+                type='number',
+                default=lambda row: row.get('net_revised', row.get('net_allocated'))
+            ),
+            dict(
+                name='amount_used',
+                description='''
+                    סכום התקציב שנוצל במהלך השנה.
+                    נקרא גם ״תקציב מבוצע״ או ״ביצוע תקציב״.
+                    סכום התקציב של הסעיף שנוצל בשקלים.
+                ''',
+                sample_values=[1000000, 5000000, 10000000],
+                type='number',
+                default=lambda row: row.get('net_executed')
+            ),
+            dict(
+                name='level',
+                description='''
+                    רמת הסעיף התקציבי.
+                    1 מייצג סעיפים ברמה הגבוהה ביותר (כמו משרדים ראשיים).
+                    4 מייצג סעיפים ברמה הנמוכה ביותר (תקנות תקציביות).
+                ''',
+                sample_values=[1, 2, 3, 4],
+                type='integer',
+                default=lambda row: len(row['code'].split('.'))
+            )
+        ]
+    )
+)
+
+def get_flow(table, params):
+    steps = []
+    source = params['source']
+    details = params['details']
+    fields = params['fields']
+    steps.append(DF.load(f'{source}/datapackage.json'))
+    DF.update_resource(-1, details=details)
+    
+    field_names = []
+    for field in fields:
+        field_name = field.pop('name')
+        field_names.append(field_name)
+        if 'default' in field:
+            default = field.pop('default')
+            datatype = field.get('type')
+            steps.append(DF.add_field(field_name, type=datatype, default=default))
+        elif 'transform' in field:
+            transform = field.pop('transform')
+            steps.append(DF.set_type(field_name, transform=transform))
+        if steps['filter']:
+            filter_func = field.pop('filter')
+            steps.append(DF.filter_rows(lambda row: filter_func(row.get(field_name))))
+        steps.append(DF.set_type(field_name, details=field))
+
+    steps.append(DF.select_fields(field_names))
+    steps.append(DF.dump_to_path(f'/var/datapackages/simpledb/{table}'))
+
+def flow(parameters, *_):
+    for table, params in PARAMETERS.items():
+        get_flow(table, params).process()
+    return DF.Flow(
+        [dict(done=True)]
+    )
