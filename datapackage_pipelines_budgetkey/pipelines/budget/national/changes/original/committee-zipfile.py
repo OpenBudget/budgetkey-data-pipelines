4aab245b604d3aecb8f75fe89af2840e7b826b4a
Adam Kariv <adam.kariv@gmail.com>
2025-03-25 21:47:02 +0200

    Download and save finance committee zipfile

---
+++ datapackage_pipelines_budgetkey/pipelines/budget/national/changes/original/committee-zipfile.py
@@ -0,0 +1,18 @@
+import dataflows as DF
+import requests
+
+YEAR = 2024
+WEIRD_ZIP_FILE = f'https://main.knesset.gov.il/Activity/committees/Finance/Documents/p{YEAR}.zip'
+OUT = '/var/datapackages/budget/national/changes/finance-committee.zip'
+
+def flow(*_):
+    # Download the zip file
+    response = requests.get(WEIRD_ZIP_FILE)
+    with open(OUT, 'wb') as f:
+        f.write(response.content)
+
+    return DF.Flow(
+        [dict(success=True)],    
+        DF.update_resource(-1, name='zip', **{'dpp:streaming': True}),
+    )
+
