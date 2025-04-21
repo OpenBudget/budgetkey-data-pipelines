bd8168ae33d039e5a8910cadf8e2852c723c5454
Adam Kariv <adam.kariv@gmail.com>
2025-04-21 13:01:23 +0300

    Bump dataflows-elasticsearch

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
                       'fuzzywuzzy[speedup]',
                       'plyvel',
                       'filemagic',
-                      'dataflows-elasticsearch>=0.0.3',
+                      'dataflows-elasticsearch>=0.1.2',
                       'datapackage-pipelines-aws',
                       'textract==1.5.0',  # later versions of textract introduce unnecessary dependency on swig
                                           # see this issue - https://github.com/deanmalmgren/textract/issues/159
