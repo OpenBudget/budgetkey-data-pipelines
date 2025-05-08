966d04f27dd23211ee250d0813fa42584761d911
Adam Kariv <adam.kariv@gmail.com>
2025-05-08 14:12:30 +0300

    Facitilies welfare

---
+++ datapackage_pipelines_budgetkey/pipelines/facilities/labor/mol.py
@@ -0,0 +1,78 @@
+import requests
+import datetime
+import dataflows as DF
+
+def get_data():
+    url = 'https://daycareclasssearch.labor.gov.il/api/FramesSearch/GetFramesList'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:136.0) Gecko/20100101 Firefox/136.0',
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'Content-Type': 'application/json',
+        'Connection': 'keep-alive',
+        'Referer': 'https://daycareclasssearch.labor.gov.il/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'Priority': 'u=0'
+    }
+
+    data = []
+    current_year = datetime.datetime.now().year
+    for frame_type in range(1, 4):
+        post_data = {"Year":current_year,"FrameType":frame_type,"FrameId":""}
+        response = requests.post(url, headers=headers, json=post_data, timeout=30)
+        if response.status_code == 200:
+            frames_list = response.json()['Frames']
+            data.extend(frames_list)
+        else:
+            print("Request failed with status code:", response.status_code)
+    return data
+
+def construct_address(record):
+    address_parts = [
+        record.get('Address', ''),
+        record.get('TownName', ''),
+    ]
+    return ', '.join(filter(None, address_parts))
+
+def scrape():
+    return DF.Flow(
+        get_data(),
+        DF.checkpoint('mol'),
+        DF.add_field('lab_symbol', 'string', lambda r: str(r['FrameId'])),
+        DF.add_field('lab_name', 'string', lambda r: r['FrameName']),
+        DF.add_field('lab_city', 'string', lambda r: r['TownName']),
+        DF.add_field('lab_address', 'string', construct_address),
+        DF.add_field('lab_manager_name', 'string', lambda r: r['ManagerName']),
+        DF.add_field('lab_phone', 'string', lambda r: r['Phone']),
+        DF.add_field('lab_org_name', 'string', lambda r: r['OrganizationName']),
+        DF.add_field('lab_facility_type', 'string', lambda r: r['FrameTypeDesc']),
+        DF.add_field('lab_daycare_type', 'string', lambda r: r['DaycareTypeDesc']),
+        DF.add_field('lab_total_places_babies', 'integer', lambda r: r['NumOfApprovedBabiesPlaces']),
+        DF.add_field('lab_available_places_babies', 'integer', lambda r: r['NumOfAvailableBabiesPlaces']),
+        DF.add_field('lab_total_places_toddlers', 'integer', lambda r: r['NumOfApprovedToddlersPlaces']),
+        DF.add_field('lab_available_places_toddlers', 'integer', lambda r: r['NumOfAvailableToddlersPlaces']),
+        DF.add_field('lab_total_places_adults', 'integer', lambda r: r['NumOfApprovedAdultsPlaces']),
+        DF.add_field('lab_available_places_adults', 'integer', lambda r: r['NumOfAvailableAdultsPlaces']),
+        DF.add_field('lab_admission_committee', 'boolean', lambda r: bool(r['IsThereAdmissionsCommittee'])),
+        DF.add_field('_id', 'string', lambda r: f'mol-{r["FrameId"]}'),
+        DF.select_fields(['_id', 'lab_.+']),
+        DF.update_resource(-1, name='mol'),
+        DF.printer()
+    )
+
+
+def flow(*_):
+    return DF.Flow(
+        scrape(),
+        DF.dump_to_path('/var/datapackages/facilities/labor'),
+        DF.dump_to_sql(dict(
+            facilities_labor={'resource-name': 'labor'}
+        )),
+        DF.update_resource(-1, **{'dpp:streaming': True}),
+    )
+
+if __name__ == '__main__':
+    scrape().process()
---
+++ datapackage_pipelines_budgetkey/pipelines/facilities/welfare/welfare.py
@@ -0,0 +1,115 @@
+# Ministry of Welfare - CKAN Datastore
+# Sample access: https://data.gov.il/api/3/action/datastore_search?resource_id=de069ddf-bcbc-4754-bda0-84873a353f7b&limit=5
+
+import requests
+import dataflows as DF
+
+def get_data():
+    # Fetch data iteratively
+    url = 'https://data.gov.il/api/3/action/datastore_search'
+    resource_id = 'de069ddf-bcbc-4754-bda0-84873a353f7b'
+    limit = 1000
+    offset = 0
+    data = []
+    types = set()
+    while True:
+        params = {'resource_id': resource_id, 'limit': limit, 'offset': offset}
+        response = requests.get(url, params=params, timeout=30)
+        if response.status_code == 200:
+            records = response.json()['result']['records']
+            if len(records) == 0:
+                break
+            for record in records:
+                if record.get('Type_Descr'):
+                    if record['Type_Descr'] not in {
+                        'מרכז הורים ילדים',
+                        'מעון יום',
+                        'מעון רב תכליתי',
+                        'מועדונית',
+                    }:
+                        continue
+                if record.get('Head_Department'):
+                    if record['Head_Department'].strip() not in {
+                        'אוטיסטים',
+                        'נוער וצעירים',
+                        'ילד ונוער',
+                    }:
+                        continue
+                if record.get('From_Age') and record.get('From_Age') > 6:
+                    continue
+                if record.get('Telephone'):
+                    record['Telephone'] = '0' + str(record['Telephone'])
+                yield record
+                # data.append(record)
+            offset += limit
+        else:
+            print("Request failed with status code:", response.status_code)
+            break
+    print(types)
+
+def organization(record):
+    kind = record['Owner_Code_Descr']
+    org = record.get('Organization')
+    if kind:
+        kind = kind.strip()
+    if kind == 'ארגון מפעיל' and org not in ('0 ללא ארגון', None):
+        return org
+    if kind in ('ציבורי', 'רשות מקומית', 'פרטי'):
+        return kind
+    assert False, f"Unknown Owner_Code_Descr: {kind!r}, org: {org!r}"
+
+def s(value):
+    if not value:
+        return None
+    if isinstance(value, str):
+        return value.strip()
+    return value
+
+def format_address(record):
+    address_parts = [
+        s(record.get('Adrees', '')),
+        s(record.get('City_Name', '')),
+    ]
+    return ', '.join(filter(None, address_parts))
+
+def scrape():
+    return DF.Flow(
+        get_data(),
+        DF.checkpoint('welfare'),
+        DF.update_schema(-1, missingValues=['לא ידוע', 'לא משויך', 'אחר', 0, '']),
+        DF.validate(),
+        # DF.filter_rows(lambda r: r['Status_des'] == 'פעילה'),
+        DF.add_field('welfare_symbol', 'string', lambda r: str(r['Misgeret_Id'])),
+        DF.add_field('welfare_name', 'string', lambda r: s(r['Name'])),
+        DF.add_field('welfare_facility_type', 'string', lambda r: s(r['Type_Descr'])),
+        DF.add_field('welfare_org', 'string', organization),
+        DF.add_field('welfare_manager_name', 'string', lambda r: s(r['Maneger_Name'])),
+        DF.add_field('welfare_city', 'string', lambda r: s(r['City_Name'])),
+        DF.add_field('welfare_address', 'string', format_address),
+        DF.add_field('welfare_phone', 'string', lambda r: s(r['Telephone'])),
+        DF.add_field('welfare_capacity', 'integer', lambda r: r['Actual_Capacity']),
+        DF.add_field('welfare_religion', 'string', lambda r: s(r['Religion'])),
+        DF.add_field('welfare_for_gender', 'string', lambda r: s(r['Gender_Descr'])),
+        DF.add_field('welfare_from_age', 'integer', lambda r: r['From_Age']),
+        DF.add_field('welfare_to_age', 'integer', lambda r: r['To_Age']),
+        DF.add_field('welfare_coord_x', 'integer', lambda r: r['GisX']),
+        DF.add_field('welfare_coord_y', 'integer', lambda r: r['GisY']),
+        DF.add_field('_id', 'string', lambda r: f'welfare-{r["welfare_symbol"]}'),
+        DF.select_fields(['_id', 'welfare_.+', 'coord_.+']),
+        DF.update_resource(-1, name='welfare'),
+        DF.dump_to_path('out/welfare'),
+        DF.printer()
+    )
+
+def flow(*_):
+    return DF.Flow(
+        scrape(),
+        DF.dump_to_path('/var/datapackages/facilities/welfare'),
+        DF.dump_to_sql(dict(
+            facilities_welfare={'resource-name': 'welfare'}
+        )),
+        DF.update_resource(-1, **{'dpp:streaming': True}),
+    )
+
+if __name__ == '__main__':
+    scrape().process()
