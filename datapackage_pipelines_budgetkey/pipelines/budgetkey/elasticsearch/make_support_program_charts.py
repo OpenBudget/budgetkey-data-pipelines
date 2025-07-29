88bed168388ed251552ef922676766abd6840596
Adam Kariv <adam.kariv@gmail.com>
2025-07-29 20:11:31 +0300

    More charts

--- datapackage_pipelines_budgetkey/pipelines/budgetkey/elasticsearch/make_support_program_charts.py
+++ datapackage_pipelines_budgetkey/pipelines/budgetkey/elasticsearch/make_support_program_charts.py
@@ -11,48 +11,77 @@ import logging
 
 
 def get_history_charts(row):
+    top_recipients = sorted(row['recipients'], key=lambda x: x['total_approved'], reverse=True)[:10]
     return dict(
         title='ניתוח היסטורי',
-        long_title='היסטוריית התמיכות בתכנית בהיבטי כספים שאושרו, שולמו ומיצוי התמיכות',
-        type='plotly',
-        layout=dict(
-            xaxis=dict(title='שנה', type='category'),
-            yaxis=dict(
-                title='סכום (₪)',
-                separatethousands=True,
-                rangemode='tozero'
+        subcharts=[
+            dict(
+                title='סכומים ששולמו ומיצוי התמיכות',
+                long_title='היסטוריית התמיכות בתכנית בהיבטי כספים שאושרו, שולמו ומיצוי התמיכות',
+                type='plotly',
+                layout=dict(
+                    xaxis=dict(title='שנה', type='category'),
+                    yaxis=dict(
+                        title='סכום (₪)',
+                        separatethousands=True,
+                        rangemode='tozero'
+                    ),
+                    yaxis2=dict(
+                        title='מיצוי התמיכות',
+                        overlaying='y',
+                        side='right',
+                        tickformat='%'
+                    )
+                ),
+                chart=[
+                    dict(
+                        mode='lines+markers',
+                        name='סכום שאושר',
+                        x=[year for year in range(row['min_year'], row['max_year'] + 1)],
+                        y=[row['per_year'].get(year, {}).get('approved', 0)
+                        for year in range(row['min_year'], row['max_year'] + 1)],
+                    ),
+                    dict(
+                        mode='lines+markers',
+                        name='סכום ששולם',
+                        x=[year for year in range(row['min_year'], row['max_year'] + 1)],
+                        y=[row['per_year'].get(year, {}).get('paid', 0)
+                        for year in range(row['min_year'], row['max_year'] + 1)],
+                    ),
+                    dict(
+                        mode='lines+markers',
+                        name='מיצוי התמיכות הממוצע',
+                        x=[year for year in range(row['min_year'], row['max_year'] + 1)],
+                        y=[row['per_year'].get(year, {}).get('utilization', 0)
+                        for year in range(row['min_year'], row['max_year'] + 1)],
+                        yaxis='y2',
+                    )
+                ],
             ),
-            yaxis2=dict(
-                title='מיצוי התמיכות',
-                overlaying='y',
-                side='right',
-                tickformat='%'
+            dict(
+                title='מקבלי התמיכה העיקריים בתכנית זו',
+                long_title='התמיכות במקבלי התמיכה העיקריים בתכנית זו, לאורך כל השנים',
+                type='plotly',
+                layout=dict(
+                    xaxis=dict(title='שנה', type='category'),
+                    yaxis=dict(
+                        title='סכום (₪)',
+                        separatethousands=True,
+                        rangemode='tozero'
+                    )
+                ),
+                chart=[
+                    dict(
+                        mode='lines+markers',
+                        name='{}'.format(x['name']),
+                        x=[year for year in range(row['min_year'], row['max_year'] + 1)],
+                        y=[x['per_year'].get(year, {}).get('approved', 0)
+                        for year in range(row['min_year'], row['max_year'] + 1)],
+                    )
+                    for x in top_recipients
+                ]
             )
-        ),
-        chart=[
-            dict(
-                mode='lines+markers',
-                name='סכום שאושר',
-                x=[year for year in range(row['min_year'], row['max_year'] + 1)],
-                y=[row['per_year'].get(year, {}).get('approved', 0)
-                   for year in range(row['min_year'], row['max_year'] + 1)],
-            ),
-            dict(
-                mode='lines+markers',
-                name='סכום ששולם',
-                x=[year for year in range(row['min_year'], row['max_year'] + 1)],
-                y=[row['per_year'].get(year, {}).get('paid', 0)
-                   for year in range(row['min_year'], row['max_year'] + 1)],
-            ),
-            dict(
-                mode='lines+markers',
-                name='מיצוי התמיכות הממוצע',
-                x=[year for year in range(row['min_year'], row['max_year'] + 1)],
-                y=[row['per_year'].get(year, {}).get('utilization', 0)
-                   for year in range(row['min_year'], row['max_year'] + 1)],
-                yaxis='y2',
-            )
-        ],
+        ]
     )
 
 def get_entity_charts(row):
@@ -69,13 +98,13 @@ def get_entity_charts(row):
                         amount=x[field],
                         amount_fmt=formatter(x[field]),
                     )
-                    for x in row['recipients']],
+                    for x in sorted(row['recipients'], key=lambda x: x[field], reverse=True)],
                     selected=0
                 )
             ) for title, field, formatter in [
                 ('לפי סך הכספים שאושרו', 'total_approved', format_number),
                 ('לפי סך הכספים ששולמו', 'total_paid', format_number),
-                ('לפי מיצוי התמיכות המוצע', 'average_utilization', format_percentage),
+                ('לפי המיצוי הממוצע של התמיכות', 'average_utilization', format_percentage),
             ]
         ]
     )
