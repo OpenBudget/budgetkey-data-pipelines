b3fda5fe91870297b087c607a83fd5c26a6320bd
Matti Picus <matti.picus@gmail.com>
2025-11-20 12:08:19 +0200

    update call_for_bids/negev-galil URL and content (#236)

--- datapackage_pipelines_budgetkey/pipelines/procurement/calls_for_bids/negev_galil.py
+++ datapackage_pipelines_budgetkey/pipelines/procurement/calls_for_bids/negev_galil.py
@@ -8,38 +8,29 @@ from datapackage_pipelines.utilities.resources import PROP_STREAMING
 
 
 def scraper():
-    URL = 'https://negev-galil.gov.il/kolotkorim/hagashat/'
-    page = pq(requests.get(URL).text)
-    tenders = page.find('tr.tenders-table')
-    for tender_ in tenders:
-        tender = pq(tender_)
-        decision = pq(tender.find('.tenders-icon img')[0]).attr('alt').split()[1]
-        page_url = pq(tender.find('a')[0]).attr('href')
-        page_title = pq(tender.find('a')[0]).text()
-        start_date = pq(tender.find('[id="date-open"]')[0]).text()
-        claim_date = pq(tender.find('[id="date-close"]')[0]).text()
+    URL = 'https://www.gov.il/CollectorsWebApi/api/DataCollector/GetResults?CollectorType=reports&CollectorType=rfp&CollectorType=drushim&CollectorType=publicsharing&limit=10&officeId=1f628453-da79-4549-8c12-1073a718c9b0&Type=7159e036-77d5-44f9-a1bf-4500e6125bf1&Status=c8ed2e3c-e5a9-4101-93f5-bae798c4ac1d&culture=he'
+    results = json.loads(requests.get(URL).text)['results']
+    for result in results:
         yield dict(
-            page_url='https://negev-galil.gov.il' + page_url,
-            page_title=page_title,
-            publisher='משרד פיתוח הפריפריה, הנגב והגליל',
-            tender_id='0',
+            page_url = 'https://gov.il' + result['url'],
+            page_title = result['title'],
+            publisher = 'משרד פיתוח הפריפריה, הנגב והגליל',
+            tender_id = result['tags']['promotedMetaData']['מספר'][0]['title'],
             tender_type='call_for_bids',
-            tender_type_he='קול קורא',
-            publication_id=None,
-            decision=decision,
-            start_date=start_date,
-            claim_date=claim_date,
-            description='',
-            documents=[]
+            tender_type_he = 'קול קורא',
+            publication_id = None,
+            start_date = result['tags']['metaData']['תאריך פרסום'][0]['title'],
+            claim_date = result['tags']['promotedMetaData']['תאריך אחרון להגשה'][0]['title'],
+            description = '',
+            documents = [],
         )
 
 
 def flow(*_):
     return DF.Flow(
         scraper(),
-        DF.filter_rows(lambda row: row['page_title'] and row['page_title'].startswith('קול קורא'), resources=-1),
-        DF.set_type('start_date', type='date', format='%d/%m/%Y', resources=-1),
-        DF.set_type('claim_date', type='datetime', format='%d/%m/%Y', resources=-1),
+        DF.set_type('start_date', type='date', format='%d.%m.%Y', resources=-1),
+        DF.set_type('claim_date', type='datetime', format='%d.%m.%Y', resources=-1),
         calculate_publication_id(9),
         DF.validate(),
         DF.update_resource(
